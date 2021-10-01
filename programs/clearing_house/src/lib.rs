6135947220002c4188394f4eeebabf985d7579fb
Chris Heaney <chrisheaney30@gmail.com>
2021-10-01 15:12:40 -0400

    protocol: move update_funding_rate to controller/funding

--- programs/clearing_house/src/lib.rs
+++ programs/clearing_house/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::max;
-
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Transfer};
 use borsh::{BorshDeserialize, BorshSerialize};
@@ -935,122 +933,11 @@ pub mod clearing_house {
     ) -> ProgramResult {
         let market =
             &mut ctx.accounts.markets.load_mut()?.markets[Markets::index_from_u64(market_index)];
-
         let price_oracle = &ctx.accounts.oracle;
-
         let clock = Clock::get().unwrap();
         let now = clock.unix_timestamp;
 
-        let time_since_last_update = now - market.amm.last_funding_rate_ts;
-
-        market.amm.last_mark_price_twap = math::amm::calculate_new_mark_twap(&market.amm, now);
-        market.amm.last_mark_price_twap_ts = now;
-
-        if time_since_last_update >= market.amm.funding_period {
-            let one_hour: u32 = 3600;
-            let period_adjustment = (24_i64)
-                .checked_mul(one_hour as i64)
-                .unwrap()
-                .checked_div(max(1, market.amm.funding_period))
-                .unwrap();
-            // funding period = 1 hour, window = 1 day
-            // low periodicity => quickly updating/settled funding rates => lower funding rate payment per interval
-            let price_spread =
-                amm::calculate_oracle_mark_spread(&market.amm, price_oracle, one_hour);
-            let funding_rate = price_spread
-                .checked_mul(FUNDING_PAYMENT_MANTISSA as i128)
-                .unwrap()
-                .checked_div(period_adjustment as i128)
-                .unwrap();
-
-            let mut haircut_numerator = 0;
-
-            if market.base_asset_amount == 0 {
-                market.amm.cumulative_funding_rate_long = market
-                    .amm
-                    .cumulative_funding_rate_long
-                    .checked_add(funding_rate)
-                    .unwrap();
-
-                market.amm.cumulative_funding_rate_short = market
-                    .amm
-                    .cumulative_funding_rate_short
-                    .checked_add(funding_rate)
-                    .unwrap();
-            } else if market.base_asset_amount > 0 {
-                // assert(market.base_asset_amount_long > market.base_asset_amount);
-                // more longs that shorts
-
-                if market.base_asset_amount_short.unsigned_abs() > 0 {
-                    haircut_numerator = market.base_asset_amount_short.unsigned_abs();
-                }
-
-                let funding_rate_long_haircut = haircut_numerator
-                    .checked_mul(MARK_PRICE_MANTISSA)
-                    .unwrap()
-                    .checked_div(market.base_asset_amount_long as u128)
-                    .unwrap();
-
-                let funding_rate_long = funding_rate
-                    .checked_mul(funding_rate_long_haircut as i128)
-                    .unwrap()
-                    .checked_div(MARK_PRICE_MANTISSA as i128)
-                    .unwrap();
-
-                market.amm.cumulative_funding_rate_long = market
-                    .amm
-                    .cumulative_funding_rate_long
-                    .checked_add(funding_rate_long)
-                    .unwrap();
-
-                market.amm.cumulative_funding_rate_short = market
-                    .amm
-                    .cumulative_funding_rate_short
-                    .checked_add(funding_rate)
-                    .unwrap();
-            } else {
-                // more shorts than longs
-                if market.base_asset_amount_long.unsigned_abs() > 0 {
-                    haircut_numerator = market.base_asset_amount_long.unsigned_abs();
-                }
-
-                let funding_rate_short_haircut = haircut_numerator
-                    .checked_mul(MARK_PRICE_MANTISSA)
-                    .unwrap()
-                    .checked_div(market.base_asset_amount_short.unsigned_abs())
-                    .unwrap();
-
-                let funding_rate_short = funding_rate
-                    .checked_mul(funding_rate_short_haircut as i128)
-                    .unwrap()
-                    .checked_div(MARK_PRICE_MANTISSA as i128)
-                    .unwrap();
-
-                market.amm.cumulative_funding_rate_short = market
-                    .amm
-                    .cumulative_funding_rate_short
-                    .checked_add(funding_rate_short)
-                    .unwrap();
-
-                market.amm.cumulative_funding_rate_long = market
-                    .amm
-                    .cumulative_funding_rate_long
-                    .checked_add(funding_rate)
-                    .unwrap();
-            }
-
-            let cum_funding_rate = market
-                .amm
-                .cumulative_funding_rate
-                .checked_add(funding_rate)
-                .unwrap();
-
-            market.amm.cumulative_funding_rate = cum_funding_rate;
-            market.amm.last_funding_rate = funding_rate;
-            market.amm.last_funding_rate_ts = now;
-            market.amm.last_mark_price_twap = market.amm.base_asset_price_with_mantissa();
-            market.amm.last_mark_price_twap_ts = now;
-        }
+        controller::funding::update_funding_rate(market, price_oracle, now);
 
         Ok(())
     }
