28da4b697b19e044bbb68a26d8d90bcc857929af
Luke Steyn <lukecaan@gmail.com>
2021-10-20 23:29:06 +1000

    Progress on stats page (#43)
    * stats page progress
    
    * Added grid to stats page, added vault/insurance/arbbot balance stats, updated SQL init scripts to match new entities,
    
    * Fixed build issues
    
    * Added open interest stat

--- sdk/src/config.ts
+++ sdk/src/config.ts
@@ -12,13 +12,13 @@ type DriftConfig = {
 
 export type DriftEnv = 'local' | 'master' | 'devnet' | 'mainnet-beta';
 
-const configs: { [key in DriftEnv]: DriftConfig } = {
+export const configs: { [key in DriftEnv]: DriftConfig } = {
 	local: {
 		ENV: 'local',
 		PYTH_ORACLE_MAPPING_ADDRESS: 'BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2',
-		CLEARING_HOUSE_PROGRAM_ID: '8Fs5E3Jt4Tx7La47XHXBWevqGrZtTJB2txvU8MrBUoWS',
-		USDC_MINT_ADDRESS: 'Doe9rajhwt18aAeaVe8vewzAsBk4kSQ2tTyZVUJhHjhY',
-		MOCK_USDC_FAUCET_ADDRESS: '2z2DLVD3tBWc86pbvvy5qN31v1NXprM6zA5MDr2FMx64',
+		CLEARING_HOUSE_PROGRAM_ID: 'H44rgda6EEuHE93aUX6FrAx6X6pNdKnBUkZiT8ssqjtG',
+		USDC_MINT_ADDRESS: '8zGuJQqwhZafTah7Uc7Z4tXRnguqkn5KLFAP8oV6PHe2',
+		MOCK_USDC_FAUCET_ADDRESS: 'AmNeSW4UMPFBodCjEJD22G3kA8EraUGkhxr3GmdyEF4f',
 		EXCHANGE_HISTORY_SERVER_URL: 'http://localhost:5000',
 		MAX_LEVERAGE: new BN(5),
 	},
