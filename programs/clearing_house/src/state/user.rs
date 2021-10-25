50caa97a18205887baf098b19b9f05d5b31420db
Chris Heaney <chrisheaney30@gmail.com>
2021-10-25 11:15:27 -0400

    protocol: add stop_loss_amount/stop_profit_amount to contract

--- programs/clearing_house/src/state/user.rs
+++ programs/clearing_house/src/state/user.rs
@@ -44,7 +44,9 @@ pub struct MarketPosition {
     pub last_cumulative_repeg_rebate: u128,
     pub last_funding_rate_ts: i64,
     pub stop_loss_price: u128,
+    pub stop_loss_amount: u128,
     pub stop_profit_price: u128,
+    pub stop_profit_amount: u128,
     pub transfer_to: Pubkey,
 
     // upgrade-ability
