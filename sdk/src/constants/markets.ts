df9f46c5908d4348cc238aa8b93c35df541d8e0e
0xbigz <83473873+0xbigz@users.noreply.github.com>
2021-10-31 21:12:19 -0400

    ui: changes with new sdk, use -PERP naming, modify stats display

--- sdk/src/constants/markets.ts
+++ sdk/src/constants/markets.ts
@@ -10,28 +10,28 @@ type Market = {
 
 const Markets: Market[] = [
 	{
-		symbol: 'SOL/USD',
+		symbol: 'SOL-PERP',
 		baseAssetSymbol: 'SOL',
 		marketIndex: new BN(0),
 		devnetPythOracle: 'J83w4HKfqxwcq3BEMMkPFSppX3gqekLyLJBexebFVkix',
 		mainnetPythOracle: 'H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG',
 	},
 	// {
-	// 	symbol: 'BTC/USD',
+	// 	symbol: 'BTC-PERP',
 	// 	baseAssetSymbol: 'BTC',
 	// 	marketIndex: new BN(1),
 	// 	devnetPythOracle: 'HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J',
 	// 	mainnetPythOracle: 'GVXRSBjFk6e6J3NbVPXohDJetcTjaeeuykUpbQF8UoMU',
 	// },
 	// {
-	// 	symbol: 'ETH/USD',
+	// 	symbol: 'ETH-PERP',
 	// 	baseAssetSymbol: 'ETH',
 	// 	marketIndex: new BN(2),
 	// 	devnetPythOracle: 'EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw',
 	// 	mainnetPythOracle: 'JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB',
 	// },
 	// {
-	// 	symbol: 'COPE/USD',
+	// 	symbol: 'COPE-PERP',
 	// 	baseAssetSymbol: 'COPE',
 	// 	marketIndex: new BN(3),
 	// 	devnetPythOracle: 'BAXDJUXtz6P5ARhHH1aPwgv4WENzHwzyhmLYK4daFwiM',
