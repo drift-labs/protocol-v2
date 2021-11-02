23f2e3393404d9d9fef94fe10ca35f8610ec53a6
Chris Heaney <chrisheaney30@gmail.com>
2021-11-01 22:27:36 -0400

    protocol/sdk: delete calculateBaseAssetPriceAsNumber

--- sdk/src/math/market.ts
+++ sdk/src/math/market.ts
@@ -1,21 +1,11 @@
 import { BN } from '@project-serum/anchor';
 import { Market } from '../types';
 import { calculatePrice } from './amm';
-import { AMM_MANTISSA } from '../constants/numericConstants';
 
 export function calculateBaseAssetPriceWithMantissa(market: Market): BN {
 	return calculatePrice(
 		market.amm.baseAssetReserve,
 		market.amm.quoteAssetReserve,
 		market.amm.pegMultiplier
-	);
-}
-
-export function calculateBaseAssetPriceAsNumber(market: Market): number {
-	const baseAssetPrice = calculateBaseAssetPriceWithMantissa(market);
-
-	return (
-		baseAssetPrice.div(AMM_MANTISSA).toNumber() +
-		baseAssetPrice.mod(AMM_MANTISSA).toNumber() / AMM_MANTISSA.toNumber()
 	);
 }
