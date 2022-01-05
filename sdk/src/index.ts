92fc814e584ab6987c33937a9aadb03ddf2b6804
Chris Heaney <chrisheaney30@gmail.com>
2022-01-05 09:23:34 -0500

    sdk: add calculateInsuranceFundSize

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -12,6 +12,7 @@ export * from './clearingHouseUser';
 export * from './clearingHouse';
 export * from './math/conversion';
 export * from './math/funding';
+export * from './math/insuranceFund';
 export * from './math/market';
 export * from './math/position';
 export * from './math/amm';
