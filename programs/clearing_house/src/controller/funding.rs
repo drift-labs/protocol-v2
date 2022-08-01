effcf6a2438367509fc756caa84ad3a3b7f340d1
0xbigz <zane@lunoho.company>
2022-08-01 11:46:32 -0400

    funding.rs: market.amm.market_position_per_lp on funding

--- programs/clearing_house/src/controller/funding.rs
+++ programs/clearing_house/src/controller/funding.rs
@@ -13,7 +13,10 @@ use crate::math::casting::{cast, cast_to_i128};
 use crate::math::constants::{
     AMM_TO_QUOTE_PRECISION_RATIO_I128, FUNDING_PAYMENT_PRECISION, ONE_HOUR,
 };
-use crate::math::funding::{calculate_funding_payment, calculate_funding_rate_long_short};
+use crate::math::funding::{
+    calculate_funding_payment, calculate_funding_payment_in_quote_precision,
+    calculate_funding_rate_long_short,
+};
 use crate::math::oracle;
 use crate::math_error;
 use crate::state::events::{FundingPaymentRecord, FundingRateRecord};
@@ -250,6 +253,19 @@ pub fn update_funding_rate(
         let (funding_rate_long, funding_rate_short, funding_imbalance_cost) =
             calculate_funding_rate_long_short(market, funding_rate)?;
 
+        market.amm.market_position_per_lp.unsettled_pnl = market
+            .amm
+            .market_position_per_lp
+            .unsettled_pnl
+            .checked_add(calculate_funding_payment_in_quote_precision(
+                if market.amm.market_position_per_lp.base_asset_amount > 0 {
+                    funding_rate_long
+                } else {
+                    funding_rate_short
+                },
+                market.amm.market_position_per_lp.base_asset_amount,
+            )?)
+            .ok_or_else(math_error!())?;
         // todo: finish robust tests
         if market.amm.curve_update_intensity > 0 {
             formulaic_update_k(
