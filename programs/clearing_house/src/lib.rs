ac3be642c16545cec020e79f6c8c108a9b3b719e
Chris Heaney <chrisheaney30@gmail.com>
2022-08-05 09:57:51 -0400

    update devnet program

--- programs/clearing_house/src/lib.rs
+++ programs/clearing_house/src/lib.rs
@@ -29,7 +29,7 @@ mod tests;
 #[cfg(feature = "mainnet-beta")]
 declare_id!("dammHkt7jmytvbS3nHTxQNEcP59aE57nxwV21YdqEDN");
 #[cfg(not(feature = "mainnet-beta"))]
-declare_id!("BMow898PH56jD8z4EaqxicoGXkR1HhN17qrER6Uc4AYq");
+declare_id!("4oyTJnAQ9FqJj1y9mPytbWsLeeHmBzGYfuFqypwyQvuh");
 
 #[program]
 pub mod clearing_house {
