7704bfd118eca2b00a8c034f6750656e254f315a
0xbigz <zane@lunoho.company>
2022-08-19 17:42:13 -0400

    add controller/insurance.rs

---
+++ programs/clearing_house/src/controller/insurance.rs
@@ -0,0 +1,52 @@
+use crate::error::{ClearingHouseResult};
+use crate::state::bank::{Bank};
+use crate::state::insurance_fund_stake::{InsuranceFundStake};
+use crate::state::user::UserStats;
+
+pub fn update_insurance_stake_balances(
+    amount: u64,
+    insurance_fund_vault_balance: u64,
+    insurance_fund_stake: &mut InsuranceFundStake,
+    user_stats: &mut UserStats,
+    bank: &mut Bank,
+) -> ClearingHouseResult {     
+    // mint = relative to the entire pool + total amount minted
+    // u128 so we can do multiply first without overflow
+    // then div and recast back
+    let amount_to_mint = if insurance_fund_vault_balance > 0 {
+        ((amount as u128)
+            .checked_mul(bank.total_lp_shares as u128)
+            .ok_or_else(math_error!())?
+            .checked_div(insurance_fund_vault_balance as u128)
+            .ok_or_else(math_error!())?) as u64
+    } else {
+        amount as u64
+    };
+
+    let n_shares = amount_to_mint as u128;
+
+    bank.total_lp_shares = bank
+        .total_lp_shares
+        .checked_add(n_shares)
+        .ok_or_else(math_error!())?;
+
+    bank.user_lp_shares = bank
+        .user_lp_shares
+        .checked_add(n_shares)
+        .ok_or_else(math_error!())?;
+
+    insurance_fund_stake.lp_shares = insurance_fund_stake
+        .lp_shares
+        .checked_add(n_shares)
+        .ok_or_else(math_error!())?;
+
+    if bank.bank_index == 0 
+    {
+        user_stats.bank_0_insurance_lp_shares = user_stats
+            .bank_0_insurance_lp_shares
+            .checked_add(n_shares)
+            .ok_or_else(math_error!())?;
+    }
+
+    Ok(())
+}
--- programs/clearing_house/src/lib.rs
+++ programs/clearing_house/src/lib.rs
@@ -988,48 +988,13 @@ pub mod clearing_house {
             remaining_accounts_iter,
         )?;
 
-        let insurance_fund_vault_balance = ctx.accounts.insurance_fund_vault.amount;
-
-        {
-            let mut bank = bank_map.get_ref_mut(&bank_index)?;
-
-            // mint = relative to the entire pool + total amount minted
-            // u128 so we can do multiply first without overflow
-            // then div and recast back
-            let amount_to_mint = if insurance_fund_vault_balance > 0 {
-                ((amount as u128)
-                    .checked_mul(bank.total_lp_shares as u128)
-                    .unwrap()
-                    .checked_div(insurance_fund_vault_balance as u128)
-                    .unwrap()) as u64
-            } else {
-                amount as u64
-            };
-
-            let n_shares = amount_to_mint as u128;
-
-            bank.total_lp_shares = bank
-                .total_lp_shares
-                .checked_add(n_shares)
-                .ok_or_else(math_error!())?;
-
-            bank.user_lp_shares = bank
-                .user_lp_shares
-                .checked_add(n_shares)
-                .ok_or_else(math_error!())?;
-
-            insurance_fund_stake.lp_shares = insurance_fund_stake
-                .lp_shares
-                .checked_add(n_shares)
-                .ok_or_else(math_error!())?;
-
-            if bank_index == 0 {
-                user_stats.bank_0_insurance_lp_shares = user_stats
-                    .bank_0_insurance_lp_shares
-                    .checked_add(n_shares)
-                    .ok_or_else(math_error!())?;
-            }
-        }
+        controller::insurance::update_insurance_stake_balances(
+            amount,
+            ctx.accounts.insurance_fund_vault.amount,
+            insurance_fund_stake,
+            user_stats,
+            &mut *bank_map.get_ref_mut(&bank_index)?,
+        )?;
 
         controller::token::receive(
             &ctx.accounts.token_program,
