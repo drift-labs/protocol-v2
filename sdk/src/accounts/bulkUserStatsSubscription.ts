7bcd3a7ab29e47bfc0e41dd7c0ab2f5039b39b5f
wphan <william@drift.trade>
2022-08-19 14:08:33 -0400

    sdk: add bulkUserStatsSubscription (#57)
    * sdk: add bulkUserStatsSubscription
    
    * nits

---
+++ sdk/src/accounts/bulkUserStatsSubscription.ts
@@ -0,0 +1,33 @@
+import { ClearingHouseUserStats } from '../clearingHouseUserStats';
+import { BulkAccountLoader } from './bulkAccountLoader';
+import { PollingUserStatsAccountSubscriber } from './pollingUserStatsAccountSubscriber';
+
+/**
+ * @param userStats
+ * @param accountLoader
+ */
+export async function bulkPollingUserStatsSubscribe(
+	userStats: ClearingHouseUserStats[],
+	accountLoader: BulkAccountLoader
+): Promise<void> {
+	if (userStats.length === 0) {
+		await accountLoader.load();
+		return;
+	}
+
+	await Promise.all(
+		userStats.map((userStat) => {
+			return (
+				userStat.accountSubscriber as PollingUserStatsAccountSubscriber
+			).addToAccountLoader();
+		})
+	);
+
+	await accountLoader.load();
+
+	await Promise.all(
+		userStats.map(async (userStat) => {
+			return userStat.subscribe();
+		})
+	);
+}
