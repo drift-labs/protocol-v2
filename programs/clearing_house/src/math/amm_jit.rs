0f20be7e5a244a942c98a0e6b667e2d266a74888
0xNineteen <0x39015319@gmail.com>
2022-09-20 13:37:22 -0400

    init

--- programs/clearing_house/src/math/amm_jit.rs
+++ programs/clearing_house/src/math/amm_jit.rs
@@ -1,33 +1,89 @@
+use crate::controller::position::PositionDirection;
 use crate::error::ClearingHouseResult;
 use crate::math::orders::standardize_base_asset_amount;
 use crate::math_error;
 use crate::state::market::PerpMarket;
 use solana_program::msg;
 
+use super::casting::cast_to_u128;
+
 // assumption: market.amm.amm_jit_is_active() == true
 // assumption: taker_baa will improve market balance (see orders.rs & amm_wants_to_make)
 pub fn calculate_jit_base_asset_amount(
     market: &PerpMarket,
     taker_base_asset_amount: u128,
+    maker_fill_price: u128,
+    valid_oracle_price: Option<i128>,
+    taker_direction: PositionDirection,
 ) -> ClearingHouseResult<u128> {
-    // simple impl
-    // todo: dynamic on imbalance
-    let mut jit_base_asset_amount = standardize_base_asset_amount(
+    let mut max_jit_amount = taker_base_asset_amount;
+
+    // check for wash trade
+    // simple impl: half max jit amount when likely to be wash
+    if let Some(oracle_price) = valid_oracle_price {
+        let oracle_price = cast_to_u128(oracle_price)?;
+
+        // maker taking a short below oracle = likely to be a wash
+        if taker_direction == PositionDirection::Long && maker_fill_price < oracle_price {
+            max_jit_amount = max_jit_amount.checked_div(2).ok_or_else(math_error!())?
+        } else if taker_direction == PositionDirection::Short && maker_fill_price > oracle_price {
+            max_jit_amount = max_jit_amount.checked_div(2).ok_or_else(math_error!())?
+        }
+    } else {
+        // todo: what to do when oracle price is invalid? probs dont take anything?
+        max_jit_amount = 0;
+    };
+
+    if max_jit_amount == 0 {
+        return Ok(0);
+    }
+
+    // check for market imbalance
+    // min/max_baa
+    let base_reserve_length = market.amm.max_base_asset_reserve - market.amm.min_base_asset_reserve;
+    let half_base_reserve_length = base_reserve_length
+        .checked_div(2)
+        .ok_or_else(math_error!())?;
+    let mid_reserve = market.amm.min_base_asset_reserve + half_base_reserve_length;
+
+    let fourth_base_reserve_length = base_reserve_length
+        .checked_div(4)
+        .ok_or_else(math_error!())?;
+    let base_reserve = market.amm.base_asset_reserve;
+
+    // min | --|-- mid --|-- | max
+    //       ^             ^
+    // if bar is in these quadrents = imbalanced so we take more baa
+    // simple impl:
+    // if we balanced we take 1/4
+    // if we not balanced we take 1/2
+
+    let damm_is_imbalanced = base_reserve <= mid_reserve - fourth_base_reserve_length
+        || base_reserve >= mid_reserve + fourth_base_reserve_length;
+    let mut jit_base_asset_amount = if damm_is_imbalanced {
         taker_base_asset_amount
             .checked_div(2)
-            .ok_or_else(math_error!())?,
+            .ok_or_else(math_error!())?
+    } else {
+        taker_base_asset_amount
+            .checked_div(4)
+            .ok_or_else(math_error!())?
+    };
+
+    if jit_base_asset_amount == 0 {
+        return Ok(0);
+    }
+
+    jit_base_asset_amount =
+        calculate_clampped_jit_base_asset_amount(market, jit_base_asset_amount)?;
+
+    jit_base_asset_amount = jit_base_asset_amount.min(max_jit_amount);
+
+    // last step we always standardize
+    jit_base_asset_amount = standardize_base_asset_amount(
+        jit_base_asset_amount,
         market.amm.base_asset_amount_step_size,
     )?;
-
-    if jit_base_asset_amount != 0 {
-        jit_base_asset_amount =
-            calculate_clampped_jit_base_asset_amount(market, jit_base_asset_amount)?;
-
-        jit_base_asset_amount = standardize_base_asset_amount(
-            jit_base_asset_amount,
-            market.amm.base_asset_amount_step_size,
-        )?;
-    }
 
     Ok(jit_base_asset_amount)
 }
