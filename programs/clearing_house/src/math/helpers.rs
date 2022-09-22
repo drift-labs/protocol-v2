dc30a98f3eed9fce6c445b076800dcc28518862c
0xNineteen <0x39015319@gmail.com>
2022-09-22 11:52:29 -0400

    improve proportion flow

--- programs/clearing_house/src/math/helpers.rs
+++ programs/clearing_house/src/math/helpers.rs
@@ -2,6 +2,7 @@ use crate::error::ClearingHouseResult;
 use crate::math::bn::U192;
 use crate::math::casting::{cast_to_i128, cast_to_u128};
 use crate::math_error;
+use num_integer::Roots;
 use solana_program::msg;
 
 pub fn standardize_value_with_remainder_i128(
@@ -40,10 +41,20 @@ pub fn get_proportion_u128(
     numerator: u128,
     denominator: u128,
 ) -> ClearingHouseResult<u128> {
+    // we use u128::max.sqrt() here
+    let large_constant = cast_to_u128(u64::MAX)?;
+
     let proportional_value = if numerator == denominator {
         value
+    } else if value >= large_constant || numerator >= large_constant {
+        let value = U192::from(value)
+            .checked_mul(U192::from(numerator))
+            .ok_or_else(math_error!())?
+            .checked_div(U192::from(denominator))
+            .ok_or_else(math_error!())?;
+
+        cast_to_u128(value)?
     } else if numerator > denominator / 2 && denominator > numerator {
-        msg!("numerator/denominator: {} / {}", numerator, denominator);
         // get values to ensure a ceiling division
         let (std_value, r) = standardize_value_with_remainder_i128(
             cast_to_i128(
@@ -69,23 +80,11 @@ pub fn get_proportion_u128(
             .checked_sub(cast_to_u128(r.signum())?)
             .ok_or_else(math_error!())?
     } else {
-        let num = value.checked_mul(numerator);
-
-        if num.is_some() {
-            // continue on
-            num.unwrap()
-                .checked_div(denominator)
-                .ok_or_else(math_error!())?
-        } else {
-            // try again with larger type
-            let value = U192::from(value)
-                .checked_mul(U192::from(numerator))
-                .ok_or_else(math_error!())?
-                .checked_div(U192::from(denominator))
-                .ok_or_else(math_error!())?;
-
-            cast_to_u128(value)?
-        }
+        value
+            .checked_mul(numerator)
+            .ok_or_else(math_error!())?
+            .checked_div(denominator)
+            .ok_or_else(math_error!())?
     };
 
     Ok(proportional_value)
