ea2b9d795b7878eccae199f7598cad07fdbae960
0xbigz <83473873+0xbigz@users.noreply.github.com>
2022-10-06 16:40:57 -0400

    cleanup

--- programs/clearing_house/src/state/perp_market_map.rs
+++ programs/clearing_house/src/state/perp_market_map.rs
@@ -33,7 +33,7 @@ impl<'a> PerpMarketMap<'a> {
         self.0
             .get(market_index)
             .ok_or_else(|| {
-                msg!("perp market not found: {}", market_index);
+                msg!("perp market mut not found: {}", market_index);
                 ErrorCode::MarketNotFound
             })?
             .load_mut()
