a47f0fc43c62f2b0dc39159cb18c360e6074b093
Evan Pipta <3pipta@gmail.com>
2022-10-12 12:06:52 -0400

    Add alpha ticket mint config values

--- sdk/src/config.ts
+++ sdk/src/config.ts
@@ -17,6 +17,7 @@ type DriftConfig = {
 	PYTH_ORACLE_MAPPING_ADDRESS: string;
 	CLEARING_HOUSE_PROGRAM_ID: string;
 	USDC_MINT_ADDRESS: string;
+	V2_ALPHA_TICKET_MINT_ADDRESS: string;
 	PERP_MARKETS: PerpMarketConfig[];
 	SPOT_MARKETS: SpotMarketConfig[];
 };
@@ -29,6 +30,8 @@ export const configs: { [key in DriftEnv]: DriftConfig } = {
 		PYTH_ORACLE_MAPPING_ADDRESS: 'BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2',
 		CLEARING_HOUSE_PROGRAM_ID: 'DUZwKJKAk2C9S88BYvQzck1M1i5hySQjxB4zW6tJ29Nw',
 		USDC_MINT_ADDRESS: '8zGuJQqwhZafTah7Uc7Z4tXRnguqkn5KLFAP8oV6PHe2',
+		V2_ALPHA_TICKET_MINT_ADDRESS:
+			'DeEiGWfCMP9psnLGkxGrBBMEAW5Jv8bBGMN8DCtFRCyB',
 		PERP_MARKETS: DevnetPerpMarkets,
 		SPOT_MARKETS: DevnetSpotMarkets,
 	},
@@ -37,6 +40,7 @@ export const configs: { [key in DriftEnv]: DriftConfig } = {
 		PYTH_ORACLE_MAPPING_ADDRESS: 'AHtgzX45WTKfkPG53L6WYhGEXwQkN1BVknET3sVsLL8J',
 		CLEARING_HOUSE_PROGRAM_ID: 'dammHkt7jmytvbS3nHTxQNEcP59aE57nxwV21YdqEDN',
 		USDC_MINT_ADDRESS: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+		V2_ALPHA_TICKET_MINT_ADDRESS: '',
 		PERP_MARKETS: MainnetMarkets,
 		SPOT_MARKETS: MainnetSpotMarkets,
 	},
