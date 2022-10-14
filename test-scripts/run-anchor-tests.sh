c7d202c8e7627ad4a99250dbe84d991f52b2e1bb
Chris Heaney <chrisheaney30@gmail.com>
2022-10-14 09:31:40 -0400

    add back missing tests

--- test-scripts/run-anchor-tests.sh
+++ test-scripts/run-anchor-tests.sh
@@ -5,6 +5,12 @@ if [ "$1" != "--skip-build" ]
 fi
 
 test_files=(
+  postOnlyAmmFulfillment.ts
+  imbalancePerpPnl.ts
+   delistMarket.ts
+   delistMarketLiq.ts
+   triggerSpotOrder.ts
+   serumTest.ts
    liquidityProvider.ts
    tradingLP.ts
    insuranceFundStake.ts
