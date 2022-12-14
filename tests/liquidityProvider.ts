e876bbff3c5019addf49ed87af15b519d1522fb0
Chris Heaney <chrisheaney30@gmail.com>
2022-12-13 21:41:50 -0500

    try fix tests/liquidityProvider.ts

--- tests/liquidityProvider.ts
+++ tests/liquidityProvider.ts
@@ -1181,6 +1181,7 @@ describe('liquidity providing', () => {
 			trader.perpPositions[0].baseAssetAmount.toString()
 		);
 
+		await driftClientUser.fetchAccounts();
 		const sdkPnl = driftClientUser.getSettledLPPosition(0)[2];
 
 		console.log('settling...');
