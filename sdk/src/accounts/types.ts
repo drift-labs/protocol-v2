32b6b98c393f6f79ea93d0c40d2e32ff748b05da
Chris Heaney <chrisheaney30@gmail.com>
2022-12-30 16:16:35 -0600

    sdk: add ability to skep fetch in subscribe for websocket subscription

--- sdk/src/accounts/types.ts
+++ sdk/src/accounts/types.ts
@@ -17,6 +17,8 @@ export interface AccountSubscriber<T> {
 	subscribe(onChange: (data: T) => void): Promise<void>;
 	fetch(): Promise<void>;
 	unsubscribe(): Promise<void>;
+
+	setData(userAccount: T): void;
 }
 
 export class NotSubscribedError extends Error {
@@ -69,7 +71,7 @@ export interface UserAccountSubscriber {
 	eventEmitter: StrictEventEmitter<EventEmitter, UserAccountEvents>;
 	isSubscribed: boolean;
 
-	subscribe(): Promise<boolean>;
+	subscribe(userAccount?: UserAccount): Promise<boolean>;
 	fetch(): Promise<void>;
 	unsubscribe(): Promise<void>;
 
