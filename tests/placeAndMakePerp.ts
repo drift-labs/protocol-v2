ad9ec8b61d80156cf9c6b2248348ec09ba4e8aaf
Chris Heaney <chrisheaney30@gmail.com>
2023-02-28 08:42:31 -0500

    add placeAndMakePerp order

---
+++ tests/placeAndMakePerp.ts
@@ -0,0 +1,215 @@
+import * as anchor from '@project-serum/anchor';
+import { assert } from 'chai';
+
+import { Program } from '@project-serum/anchor';
+
+import { Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+import {
+	TestClient,
+	BN,
+	PRICE_PRECISION,
+	TestClient,
+	PositionDirection,
+	User,
+	Wallet,
+	EventSubscriber,
+	BASE_PRECISION,
+	getLimitOrderParams,
+	OracleSource,
+} from '../sdk/src';
+
+import {
+	initializeQuoteSpotMarket,
+	initializeSolSpotMarket,
+	mockOracle,
+	mockUSDCMint,
+	mockUserUSDCAccount,
+	printTxLogs,
+	sleep,
+} from './testHelpers';
+import { BulkAccountLoader, PEG_PRECISION, PostOnlyParams } from '../sdk';
+
+describe('place and make perp order', () => {
+	const provider = anchor.AnchorProvider.local(undefined, {
+		commitment: 'confirmed',
+		preflightCommitment: 'confirmed',
+	});
+	const connection = provider.connection;
+	anchor.setProvider(provider);
+	const chProgram = anchor.workspace.Drift as Program;
+
+	let makerDriftClient: TestClient;
+	let makerDriftClientUser: User;
+	const eventSubscriber = new EventSubscriber(connection, chProgram, {
+		commitment: 'recent',
+	});
+	eventSubscriber.subscribe();
+
+	const bulkAccountLoader = new BulkAccountLoader(connection, 'confirmed', 1);
+
+	// ammInvariant == k == x * y
+	const mantissaSqrtScale = new BN(Math.sqrt(PRICE_PRECISION.toNumber()));
+	const ammInitialQuoteAssetReserve = new anchor.BN(5 * 10 ** 13).mul(
+		mantissaSqrtScale
+	);
+	const ammInitialBaseAssetReserve = new anchor.BN(5 * 10 ** 13).mul(
+		mantissaSqrtScale
+	);
+
+	let usdcMint;
+	let userUSDCAccount;
+
+	const usdcAmount = new BN(100 * 10 ** 6);
+
+	let solUsd;
+	let marketIndexes;
+	let spotMarketIndexes;
+	let oracleInfos;
+
+	before(async () => {
+		usdcMint = await mockUSDCMint(provider);
+		userUSDCAccount = await mockUserUSDCAccount(usdcMint, usdcAmount, provider);
+
+		solUsd = await mockOracle(32.821);
+
+		marketIndexes = [0];
+		spotMarketIndexes = [0, 1];
+		oracleInfos = [{ publicKey: solUsd, source: OracleSource.PYTH }];
+
+		makerDriftClient = new TestClient({
+			connection,
+			wallet: provider.wallet,
+			programID: chProgram.programId,
+			opts: {
+				commitment: 'confirmed',
+			},
+			activeSubAccountId: 0,
+			perpMarketIndexes: marketIndexes,
+			spotMarketIndexes: spotMarketIndexes,
+			oracleInfos,
+			accountSubscription: {
+				type: 'polling',
+				accountLoader: bulkAccountLoader,
+			},
+		});
+		await makerDriftClient.initialize(usdcMint.publicKey, true);
+		await makerDriftClient.subscribe();
+		await initializeQuoteSpotMarket(makerDriftClient, usdcMint.publicKey);
+
+		const periodicity = new BN(0);
+		await makerDriftClient.initializePerpMarket(
+			solUsd,
+			ammInitialBaseAssetReserve,
+			ammInitialQuoteAssetReserve,
+			periodicity,
+			new BN(32 * PEG_PRECISION.toNumber())
+		);
+
+		await makerDriftClient.initializeUserAccountAndDepositCollateral(
+			usdcAmount,
+			userUSDCAccount.publicKey
+		);
+
+		makerDriftClientUser = new User({
+			driftClient: makerDriftClient,
+			userAccountPublicKey: await makerDriftClient.getUserAccountPublicKey(),
+		});
+		await makerDriftClientUser.subscribe();
+	});
+
+	after(async () => {
+		await makerDriftClient.unsubscribe();
+		await makerDriftClientUser.unsubscribe();
+		await eventSubscriber.unsubscribe();
+	});
+
+	it('make', async () => {
+		const keypair = new Keypair();
+		await provider.connection.requestAirdrop(keypair.publicKey, 10 ** 9);
+		await sleep(1000);
+		const wallet = new Wallet(keypair);
+		const userUSDCAccount = await mockUserUSDCAccount(
+			usdcMint,
+			usdcAmount,
+			provider,
+			keypair.publicKey
+		);
+		const takerDriftClient = new TestClient({
+			connection,
+			wallet,
+			programID: chProgram.programId,
+			opts: {
+				commitment: 'confirmed',
+			},
+			activeSubAccountId: 0,
+			perpMarketIndexes: marketIndexes,
+			spotMarketIndexes: spotMarketIndexes,
+			oracleInfos,
+			userStats: true,
+			accountSubscription: {
+				type: 'polling',
+				accountLoader: bulkAccountLoader,
+			},
+		});
+		await takerDriftClient.subscribe();
+		await takerDriftClient.initializeUserAccountAndDepositCollateral(
+			usdcAmount,
+			userUSDCAccount.publicKey
+		);
+		const takerDriftClientUser = new User({
+			driftClient: takerDriftClient,
+			userAccountPublicKey: await takerDriftClient.getUserAccountPublicKey(),
+		});
+		await takerDriftClientUser.subscribe();
+
+		const marketIndex = 0;
+		const baseAssetAmount = BASE_PRECISION;
+		const takerOrderParams = getLimitOrderParams({
+			marketIndex,
+			direction: PositionDirection.LONG,
+			baseAssetAmount,
+			price: new BN(34).mul(PRICE_PRECISION),
+			auctionStartPrice: new BN(33).mul(PRICE_PRECISION),
+			auctionEndPrice: new BN(34).mul(PRICE_PRECISION),
+			auctionDuration: 10,
+			userOrderId: 1,
+			postOnly: PostOnlyParams.NONE,
+		});
+		await takerDriftClient.placePerpOrder(takerOrderParams);
+		await takerDriftClientUser.fetchAccounts();
+		const order = takerDriftClientUser.getOrderByUserOrderId(1);
+		assert(!order.postOnly);
+
+		const makerOrderParams = getLimitOrderParams({
+			marketIndex,
+			direction: PositionDirection.SHORT,
+			baseAssetAmount,
+			price: new BN(33).mul(PRICE_PRECISION),
+			userOrderId: 1,
+			postOnly: PostOnlyParams.MUST_POST_ONLY,
+			immediateOrCancel: true,
+		});
+
+		const txSig = await makerDriftClient.placeAndMakePerpOrder(
+			makerOrderParams,
+			{
+				taker: await takerDriftClient.getUserAccountPublicKey(),
+				order: takerDriftClient.getOrderByUserId(1),
+				takerUserAccount: takerDriftClient.getUserAccount(),
+				takerStats: takerDriftClient.getUserStatsAccountPublicKey(),
+			}
+		);
+
+		await printTxLogs(connection, txSig);
+
+		const makerPosition = makerDriftClient.getUser().getPerpPosition(0);
+		assert(makerPosition.baseAssetAmount.eq(BASE_PRECISION.neg()));
+
+		const takerPosition = takerDriftClient.getUser().getPerpPosition(0);
+		assert(takerPosition.baseAssetAmount.eq(BASE_PRECISION));
+
+		await takerDriftClientUser.unsubscribe();
+		await takerDriftClient.unsubscribe();
+	});
+});
