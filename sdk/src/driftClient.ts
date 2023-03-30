f712769fdf821f4c03b96509bc45d369f77d366a
Evan Pipta <3pipta@gmail.com>
2023-03-30 17:18:50 -0400

    sdk: add method to bundle if stake account creation and deposit

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -161,10 +161,10 @@ export class DriftClient {
 				? {
 						type: 'polling',
 						accountLoader: config.accountSubscription.accountLoader,
-				  }
+					}
 				: {
 						type: 'websocket',
-				  };
+					};
 		this.createUsers(subAccountIds, this.userAccountSubscriptionConfig);
 		if (config.userStats) {
 			this.userStats = new UserStats({
@@ -1332,7 +1332,7 @@ export class DriftClient {
 					this.program.programId,
 					this.authority,
 					subAccountId
-			  )
+				)
 			: await this.getUserAccountPublicKey();
 
 		let remainingAccounts = [];
@@ -1523,7 +1523,7 @@ export class DriftClient {
 						marketIndex,
 						fromSubAccountId,
 						subAccountId
-				  )
+					)
 				: await this.getDepositInstruction(
 						amount,
 						marketIndex,
@@ -1531,7 +1531,7 @@ export class DriftClient {
 						subAccountId,
 						false,
 						false
-				  );
+					);
 
 		if (subAccountId === 0) {
 			if (
@@ -2490,7 +2490,7 @@ export class DriftClient {
 			? order.marketIndex
 			: userAccount.orders.find(
 					(order) => order.orderId === userAccount.nextOrderId - 1
-			  ).marketIndex;
+				).marketIndex;
 
 		makerInfo = Array.isArray(makerInfo)
 			? makerInfo
@@ -2658,7 +2658,7 @@ export class DriftClient {
 			? order.marketIndex
 			: userAccount.orders.find(
 					(order) => order.orderId === userAccount.nextOrderId - 1
-			  ).marketIndex;
+				).marketIndex;
 
 		const userAccounts = [userAccount];
 		if (makerInfo !== undefined) {
@@ -4228,6 +4228,21 @@ export class DriftClient {
 		amount: BN,
 		collateralAccountPublicKey: PublicKey
 	): Promise<TransactionSignature> {
+		const { txSig } = await this.sendTransaction(
+			wrapInTx(
+				await this.getAddInsuranceFundStakeIx(marketIndex, amount, collateralAccountPublicKey),
+			),
+			[],
+			this.opts
+		);
+		return txSig;
+	}
+
+	public async getAddInsuranceFundStakeIx(
+		marketIndex: number,
+		amount: BN,
+		collateralAccountPublicKey: PublicKey
+	): Promise<TransactionInstruction> {
 		const spotMarket = this.getSpotMarketAccount(marketIndex);
 		const ifStakeAccountPublicKey = getInsuranceFundStakeAccountPublicKey(
 			this.program.programId,
@@ -4241,7 +4256,7 @@ export class DriftClient {
 			writableSpotMarketIndexes: [marketIndex],
 		});
 
-		const tx = await this.program.transaction.addInsuranceFundStake(
+		const ix = await this.program.instruction.addInsuranceFundStake(
 			marketIndex,
 			amount,
 			{
@@ -4261,7 +4276,36 @@ export class DriftClient {
 			}
 		);
 
+		return ix;
+	}
+
+	/**
+	 * Bundles instructions for initializeInsuranceFundStake and addInsuranceFundStake in one tx
+	 */
+	public async initializeInsuranceFundStakeAndAddFunds(
+		marketIndex: number,
+		amount: BN,
+		collateralAccountPublicKey: PublicKey,
+		txParams?: TxParams
+	): Promise<TransactionSignature> {
+
+		const tx = new Transaction();
+
+		const instructions = [
+			await this.getInitializeInsuranceFundStakeIx(marketIndex),
+			await this.getAddInsuranceFundStakeIx(marketIndex, amount, collateralAccountPublicKey)
+		];
+
+		console.log(instructions);
+
+		tx.add(...instructions);
+
+		// Wat to do with these when bundleinng?
+		// txParams?.computeUnits,
+		// txParams?.computeUnitsPrice
+
 		const { txSig } = await this.sendTransaction(tx, [], this.opts);
+
 		return txSig;
 	}
 
