f4f01a67a6d5143b15c6e6a490d6046190a0c4a3
Chris Heaney <chrisheaney30@gmail.com>
2023-04-14 09:51:51 -0400

    program: move max number of usres to 5000

--- programs/drift/src/instructions/user.rs
+++ programs/drift/src/instructions/user.rs
@@ -118,7 +118,7 @@ pub fn handle_initialize_user(
     safe_increment!(state.number_of_sub_accounts, 1);
 
     validate!(
-        state.number_of_sub_accounts <= 3000,
+        state.number_of_sub_accounts <= 5000,
         ErrorCode::MaxNumberOfUsers
     )?;
 
