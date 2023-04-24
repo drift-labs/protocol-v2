199779cd0c9351460b2f5052649df1d49b2dc51d
Chris Heaney <chrisheaney30@gmail.com>
2023-04-24 17:04:15 -0400

    vamm order prototype

---
+++ sdk/src/dlob/fallbackLiquidity.ts
@@ -0,0 +1,125 @@
+import { BN } from '@coral-xyz/anchor';
+import { PerpMarketAccount, SwapDirection } from '../types';
+import {
+	calculateAmmReservesAfterSwap,
+	calculateMarketOpenBidAsk,
+	calculateQuoteAssetAmountSwapped,
+	calculateSpreadReserves,
+	calculateUpdatedAMM,
+} from '../math/amm';
+import { OraclePriceData } from '../oracles/types';
+import { BASE_PRECISION } from '../constants/numericConstants';
+
+export interface FallbackOrders {
+	getBids(): Generator<{ price: BN; size: BN }>;
+	getAsks(): Generator<{ price: BN; size: BN }>;
+}
+
+export function vammFallbackOrders({
+	marketAccount,
+	oraclePriceData,
+	numOrders,
+	now,
+}: {
+	marketAccount: PerpMarketAccount;
+	oraclePriceData: OraclePriceData;
+	numOrders: number;
+	now?: BN;
+}) {
+	const updatedAmm = calculateUpdatedAMM(marketAccount.amm, oraclePriceData);
+
+	const [openBids, openAsks] = calculateMarketOpenBidAsk(
+		updatedAmm.baseAssetReserve,
+		updatedAmm.minBaseAssetReserve,
+		updatedAmm.maxBaseAssetReserve,
+		updatedAmm.orderStepSize
+	);
+
+	now = now ?? new BN(Date.now() / 1000);
+	const [bidReserves, askReserves] = calculateSpreadReserves(
+		updatedAmm,
+		oraclePriceData,
+		now
+	);
+
+	let numBids = 0;
+	const baseSize = openBids.div(new BN(numOrders));
+	const bidAmm = {
+		baseAssetReserve: bidReserves.baseAssetReserve,
+		quoteAssetReserve: bidReserves.quoteAssetReserve,
+		sqrtK: updatedAmm.sqrtK,
+		pegMultiplier: updatedAmm.pegMultiplier,
+	};
+	const getBids = function* () {
+		while (numBids < numOrders) {
+			const [afterSwapQuoteReserves, afterSwapBaseReserves] =
+				calculateAmmReservesAfterSwap(
+					bidAmm,
+					'base',
+					baseSize,
+					SwapDirection.ADD
+				);
+
+			const quoteSwapped = calculateQuoteAssetAmountSwapped(
+				bidAmm.quoteAssetReserve.sub(afterSwapQuoteReserves).abs(),
+				bidAmm.pegMultiplier,
+				SwapDirection.ADD
+			);
+
+			const price = quoteSwapped.mul(BASE_PRECISION).div(baseSize);
+
+			bidAmm.baseAssetReserve = afterSwapBaseReserves;
+			bidAmm.quoteAssetReserve = afterSwapQuoteReserves;
+
+			yield {
+				price,
+				size: baseSize,
+			};
+
+			numBids++;
+		}
+	};
+
+	let numAsks = 0;
+	const askSize = openAsks.abs().div(new BN(numOrders));
+	const askAmm = {
+		baseAssetReserve: askReserves.baseAssetReserve,
+		quoteAssetReserve: askReserves.quoteAssetReserve,
+		sqrtK: updatedAmm.sqrtK,
+		pegMultiplier: updatedAmm.pegMultiplier,
+	};
+	const getAsks = function* () {
+		while (numAsks < numOrders) {
+			const [afterSwapQuoteReserves, afterSwapBaseReserves] =
+				calculateAmmReservesAfterSwap(
+					askAmm,
+					'base',
+					askSize,
+					SwapDirection.REMOVE
+				);
+
+			const quoteSwapped = calculateQuoteAssetAmountSwapped(
+				askAmm.quoteAssetReserve.sub(afterSwapQuoteReserves).abs(),
+				askAmm.pegMultiplier,
+				SwapDirection.REMOVE
+			);
+
+			const price = quoteSwapped.mul(BASE_PRECISION).div(askSize);
+
+			askAmm.baseAssetReserve = afterSwapBaseReserves;
+			askAmm.quoteAssetReserve = afterSwapQuoteReserves;
+
+			yield {
+				price,
+				size: askSize,
+			};
+
+			numAsks++;
+		}
+	};
+
+	return {
+		getBids,
+		getAsks,
+	};
+}
