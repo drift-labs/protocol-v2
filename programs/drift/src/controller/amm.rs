de82b75e12aeb2261001ac20837c1115c17e1abe
0xbigz <83473873+0xbigz@users.noreply.github.com>
2023-04-26 22:20:11 -0400

    bigz/fix-amm-revenue-pool-transfer-last-settle-ts

--- programs/drift/src/controller/amm.rs
+++ programs/drift/src/controller/amm.rs
@@ -383,6 +383,88 @@ pub fn get_fee_pool_tokens(
     .cast()
 }
 
+fn calculate_revenue_pool_transfer(
+    market: &PerpMarket,
+    spot_market: &SpotMarket,
+    amm_fee_pool_token_amount_after: u128,
+    terminal_state_surplus: i128,
+) -> DriftResult<u128> {
+    if terminal_state_surplus < FEE_POOL_TO_REVENUE_POOL_THRESHOLD.cast()? {
+        let max_revenue_withdraw_allowed = market
+            .insurance_claim
+            .max_revenue_withdraw_per_period
+            .cast::<i64>()?
+            .safe_sub(market.insurance_claim.revenue_withdraw_since_last_settle)?
+            .cast::<u128>()?;
+
+        if max_revenue_withdraw_allowed > 0 {
+            let spot_market_revenue_pool_amount = get_token_amount(
+                spot_market.revenue_pool.scaled_balance,
+                spot_market,
+                &SpotBalanceType::Deposit,
+            )?;
+
+            let revenue_pool_transfer: u128 = terminal_state_surplus
+                .safe_sub(FEE_POOL_TO_REVENUE_POOL_THRESHOLD.cast()?)?
+                .unsigned_abs()
+                .min(spot_market_revenue_pool_amount)
+                .min(max_revenue_withdraw_allowed);
+
+            Ok(revenue_pool_transfer.cast()?)
+        } else {
+            Ok(0)
+        }
+    } else {
+        let fee_pool_threshold = amm_fee_pool_token_amount_after
+            .saturating_sub(
+                FEE_POOL_TO_REVENUE_POOL_THRESHOLD.safe_add(market.amm.total_social_loss)?,
+            )
+            .cast()?;
+
+        let total_liq_fees_for_revenue_pool: i128 = market
+            .amm
+            .total_liquidation_fee
+            .min(
+                market
+                    .insurance_claim
+                    .quote_settled_insurance
+                    .safe_add(market.insurance_claim.quote_max_insurance)?
+                    .cast()?,
+            )
+            .cast()?;
+
+        let max_revenue_to_settle = market
+            .insurance_claim
+            .revenue_withdraw_since_last_settle
+            .safe_add(
+                market
+                    .insurance_claim
+                    .max_revenue_withdraw_per_period
+                    .cast()?,
+            )?
+            .min(market.amm.net_revenue_since_last_funding)
+            .max(0);
+
+        let total_fee_for_if = get_total_fee_lower_bound(market)?.cast::<i128>()?;
+
+        let revenue_pool_transfer = total_fee_for_if
+            .safe_add(total_liq_fees_for_revenue_pool)?
+            .safe_sub(market.amm.total_fee_withdrawn.cast()?)?
+            .max(0)
+            .min(fee_pool_threshold)
+            .min(max_revenue_to_settle.cast()?);
+
+        validate!(
+            revenue_pool_transfer >= 0,
+            ErrorCode::InsufficientPerpPnlPool,
+            "revenue_pool_transfer negative ({})",
+            revenue_pool_transfer
+        )?;
+
+        Ok(revenue_pool_transfer.unsigned_abs())
+    }
+}
+
 pub fn update_pool_balances(
     market: &mut PerpMarket,
     spot_market: &mut SpotMarket,
@@ -466,121 +548,63 @@ pub fn update_pool_balances(
             .total_fee_minus_distributions
             .safe_sub(market.amm.total_fee_withdrawn.cast()?)?;
 
-        if terminal_state_surplus < FEE_POOL_TO_REVENUE_POOL_THRESHOLD.cast()? {
-            // market can perform withdraw from revenue pool
-            if spot_market.insurance_fund.last_revenue_settle_ts
-                > market.insurance_claim.last_revenue_withdraw_ts
-            {
-                validate!(now >= market.insurance_claim.last_revenue_withdraw_ts && now >= spot_market.insurance_fund.last_revenue_settle_ts,
-                    ErrorCode::BlockchainClockInconsistency,
-                    "issue with clock unix timestamp {} < market.insurance_claim.last_revenue_withdraw_ts={}/spot_market.last_revenue_settle_ts={}",
-                    now,
-                    market.insurance_claim.last_revenue_withdraw_ts,
-                    spot_market.insurance_fund.last_revenue_settle_ts,
-                )?;
-                market.insurance_claim.revenue_withdraw_since_last_settle = 0;
+        // market can perform withdraw from revenue pool
+        if spot_market.insurance_fund.last_revenue_settle_ts
+            > market.insurance_claim.last_revenue_withdraw_ts
+        {
+            validate!(now >= market.insurance_claim.last_revenue_withdraw_ts && now >= spot_market.insurance_fund.last_revenue_settle_ts,
+                ErrorCode::BlockchainClockInconsistency,
+                "issue with clock unix timestamp {} < market.insurance_claim.last_revenue_withdraw_ts={}/spot_market.last_revenue_settle_ts={}",
+                now,
+                market.insurance_claim.last_revenue_withdraw_ts,
+                spot_market.insurance_fund.last_revenue_settle_ts,
+            )?;
+            market.insurance_claim.revenue_withdraw_since_last_settle = 0;
+        }
+
+        let revenue_pool_transfer = calculate_revenue_pool_transfer(
+            &market,
+            &spot_market,
+            amm_fee_pool_token_amount_after,
+            terminal_state_surplus,
+        )?;
+
+        if revenue_pool_transfer > 0 {
+            if market.amm.total_fee_minus_distributions > 0 {
+                market.amm.total_fee_minus_distributions = market
+                    .amm
+                    .total_fee_minus_distributions
+                    .safe_add(revenue_pool_transfer.cast::<i128>()?)?;
             }
 
-            let max_revenue_withdraw_allowed = market
-                .insurance_claim
-                .max_revenue_withdraw_per_period
-                .cast::<i64>()?
-                .safe_sub(market.insurance_claim.revenue_withdraw_since_last_settle)?
-                .cast::<u128>()?;
-
-            if max_revenue_withdraw_allowed > 0 {
-                let spot_market_revenue_pool_amount = get_token_amount(
-                    spot_market.revenue_pool.scaled_balance,
-                    spot_market,
-                    &SpotBalanceType::Deposit,
-                )?;
-
-                let revenue_pool_transfer: u128 = terminal_state_surplus
-                    .safe_sub(FEE_POOL_TO_REVENUE_POOL_THRESHOLD.cast()?)?
-                    .unsigned_abs()
-                    .min(spot_market_revenue_pool_amount)
-                    .min(max_revenue_withdraw_allowed);
-
+            if terminal_state_surplus < FEE_POOL_TO_REVENUE_POOL_THRESHOLD.cast()? {
                 transfer_revenue_pool_to_spot_balance(
                     revenue_pool_transfer,
                     spot_market,
                     &mut market.amm.fee_pool,
                 )?;
 
-                market.amm.total_fee_minus_distributions = market
-                    .amm
-                    .total_fee_minus_distributions
-                    .safe_add(revenue_pool_transfer.cast::<i128>()?)?;
-
                 market.insurance_claim.revenue_withdraw_since_last_settle = market
                     .insurance_claim
                     .revenue_withdraw_since_last_settle
                     .safe_add(revenue_pool_transfer.cast()?)?;
+            } else {
+                transfer_spot_balance_to_revenue_pool(
+                    revenue_pool_transfer,
+                    spot_market,
+                    &mut market.amm.fee_pool,
+                )?;
+                market.insurance_claim.revenue_withdraw_since_last_settle = market
+                    .insurance_claim
+                    .revenue_withdraw_since_last_settle
+                    .safe_sub(revenue_pool_transfer.cast()?)?;
 
-                market.insurance_claim.last_revenue_withdraw_ts = now;
+                market.amm.total_fee_withdrawn = market
+                    .amm
+                    .total_fee_withdrawn
+                    .safe_add(revenue_pool_transfer)?;
             }
-        } else {
-            let fee_pool_threshold = amm_fee_pool_token_amount_after
-                .saturating_sub(
-                    FEE_POOL_TO_REVENUE_POOL_THRESHOLD.safe_add(market.amm.total_social_loss)?,
-                )
-                .cast()?;
-
-            let total_liq_fees_for_revenue_pool: i128 = market
-                .amm
-                .total_liquidation_fee
-                .min(
-                    market
-                        .insurance_claim
-                        .quote_settled_insurance
-                        .safe_add(market.insurance_claim.quote_max_insurance)?
-                        .cast()?,
-                )
-                .cast()?;
-
-            let max_revenue_to_settle = market
-                .insurance_claim
-                .revenue_withdraw_since_last_settle
-                .safe_add(
-                    market
-                        .insurance_claim
-                        .max_revenue_withdraw_per_period
-                        .cast()?,
-                )?
-                .min(market.amm.net_revenue_since_last_funding)
-                .max(0);
-
-            let total_fee_for_if = get_total_fee_lower_bound(market)?.cast::<i128>()?;
-
-            let revenue_pool_transfer = total_fee_for_if
-                .safe_add(total_liq_fees_for_revenue_pool)?
-                .safe_sub(market.amm.total_fee_withdrawn.cast()?)?
-                .max(0)
-                .min(fee_pool_threshold)
-                .min(max_revenue_to_settle.cast()?);
-
-            validate!(
-                revenue_pool_transfer >= 0,
-                ErrorCode::InsufficientPerpPnlPool,
-                "revenue_pool_transfer negative ({})",
-                revenue_pool_transfer,
-            )?;
-
-            transfer_spot_balance_to_revenue_pool(
-                revenue_pool_transfer.unsigned_abs(),
-                spot_market,
-                &mut market.amm.fee_pool,
-            )?;
-
-            market.insurance_claim.revenue_withdraw_since_last_settle = market
-                .insurance_claim
-                .revenue_withdraw_since_last_settle
-                .safe_sub(revenue_pool_transfer.cast()?)?;
-
-            market.amm.total_fee_withdrawn = market
-                .amm
-                .total_fee_withdrawn
-                .safe_add(revenue_pool_transfer.unsigned_abs())?;
+            market.insurance_claim.last_revenue_withdraw_ts = now;
         }
     }
 
