f7df89746ee5ff44d469c6d6ea8f46205d85af2d
0xbigz <83473873+0xbigz@users.noreply.github.com>
2023-05-04 14:10:03 -0400

    add AMM comments

--- programs/drift/src/state/perp_market.rs
+++ programs/drift/src/state/perp_market.rs
@@ -435,81 +435,160 @@ impl SpotBalance for PoolBalance {
 #[derive(Debug, PartialEq, Eq)]
 #[repr(C)]
 pub struct AMM {
+    /// oracle price data public key
     pub oracle: Pubkey,
+    /// stores historically witnessed oracle data
     pub historical_oracle_data: HistoricalOracleData,
+    /// accumulated base asset amount since inception per lp share
     pub base_asset_amount_per_lp: i128,
+    /// accumulated quote asset amount since inception per lp share
     pub quote_asset_amount_per_lp: i128,
+    /// partition of fees from perp market trading moved from pnl settlements
     pub fee_pool: PoolBalance,
+    /// `x` reserves for constant product mm formula (x * y = k)
     pub base_asset_reserve: u128,
+    /// `y` reserves for constant product mm formula (x * y = k)
     pub quote_asset_reserve: u128,
+    /// determines how close the min/max base asset reserve sit vs base reserves
+    /// allow for decreasing slippage without increasing liquidity and v.v.
     pub concentration_coef: u128,
+    /// minimum base_asset_reserve allowed before AMM is unavailable
     pub min_base_asset_reserve: u128,
+    /// maximum base_asset_reserve allowed before AMM is unavailable
     pub max_base_asset_reserve: u128,
+    /// `sqrt(k)` in constant product mm formula (x * y = k). stored to avoid drift caused by integer math issues
     pub sqrt_k: u128,
+    /// normalizing numerical factor for y, its use offers lowest slippage in cp-curve when market is balanced
     pub peg_multiplier: u128,
+    /// y when market is balanced. stored to save computation
     pub terminal_quote_asset_reserve: u128,
+    /// tracks number of total longs in market (regardless of counterparty)
     pub base_asset_amount_long: i128,
+    /// tracks number of total shorts in market (regardless of counterparty)
     pub base_asset_amount_short: i128,
+    /// tracks net position (longs-shorts) in market with AMM as counterparty
     pub base_asset_amount_with_amm: i128,
+    /// tracks net position (longs-shorts) in market with LPs as counterparty
     pub base_asset_amount_with_unsettled_lp: i128,
+    /// max allowed open interest, blocks trades that breach this value
     pub max_open_interest: u128,
+    /// sum of all user's perp quote_asset_amount in market
     pub quote_asset_amount: i128,
+    /// sum of all long user's quote_entry_amount in market
     pub quote_entry_amount_long: i128,
+    /// sum of all short user's quote_entry_amount in market
     pub quote_entry_amount_short: i128,
+    /// sum of all long user's quote_break_even_amount in market
     pub quote_break_even_amount_long: i128,
+    /// sum of all short user's quote_break_even_amount in market
     pub quote_break_even_amount_short: i128,
+    /// total user lp shares of sqrt_k (protocol owned liquidity = sqrt_k - last_funding_rate)
     pub user_lp_shares: u128,
+    /// last funding rate in this perp market (unit is quote per base)
     pub last_funding_rate: i64,
+    /// last funding rate for longs in this perp market (unit is quote per base)
     pub last_funding_rate_long: i64,
+    /// last funding rate for shorts in this perp market (unit is quote per base)
     pub last_funding_rate_short: i64,
+    /// estimate of last 24h of funding rate perp market (unit is quote per base)
     pub last_24h_avg_funding_rate: i64,
+    /// total fees collected by this perp market
     pub total_fee: i128,
+    /// total fees collected by the vAMM's bid/ask spread
     pub total_mm_fee: i128,
+    /// total fees collected by exchange fee schedule
     pub total_exchange_fee: u128,
+    /// total fees minus any recognized upnl and pool withdraws
     pub total_fee_minus_distributions: i128,
+    /// sum of all fees from fee pool withdrawn to revenue pool
     pub total_fee_withdrawn: u128,
+    /// all fees collected by market for liquidations
     pub total_liquidation_fee: u128,
+    /// accumulated funding rate for longs since inception in market
     pub cumulative_funding_rate_long: i128,
+    /// accumulated funding rate for shorts since inception in market
     pub cumulative_funding_rate_short: i128,
+    /// accumulated social loss paid by users since inception in market
     pub total_social_loss: u128,
+    /// transformed base_asset_reserve for users going long
     pub ask_base_asset_reserve: u128,
+    /// transformed quote_asset_reserve for users going long
     pub ask_quote_asset_reserve: u128,
+    /// transformed base_asset_reserve for users going short
     pub bid_base_asset_reserve: u128,
+    /// transformed quote_asset_reserve for users going short
     pub bid_quote_asset_reserve: u128,
+    /// the last seen oracle price partially shrunk toward the amm reserve price
     pub last_oracle_normalised_price: i64,
+    /// the gap between the oracle price and the reserve price = y * peg_multiplier / x
     pub last_oracle_reserve_price_spread_pct: i64,
+    /// average estimate of bid price over funding_period
     pub last_bid_price_twap: u64,
+    /// average estimate of ask price over funding_period
     pub last_ask_price_twap: u64,
+    /// average estimate of (bid+ask)/2 price over funding_period
     pub last_mark_price_twap: u64,
+    /// average estimate of (bid+ask)/2 price over FIVE_MINUTES
     pub last_mark_price_twap_5min: u64,
+    /// the last blockchain slot the amm was updated
     pub last_update_slot: u64,
+    /// the pct size of the oracle confidence interval
+    /// PERCENTAGE_PRECISION
     pub last_oracle_conf_pct: u64,
+    /// the total_fee_minus_distribution change since the last funding update
     pub net_revenue_since_last_funding: i64,
+    /// the last funding rate update unix_timestamp
     pub last_funding_rate_ts: i64,
+    /// the peridocity of the funding rate updates
     pub funding_period: i64,
+    /// the base step size (increment) of orders
     pub order_step_size: u64,
+    /// the price tick size of orders
     pub order_tick_size: u64,
+    /// the minimum base size of an order
     pub min_order_size: u64,
+    /// the max base size a single user can have
     pub max_position_size: u64,
+    /// estimated total of volume in market
+    /// QUOTE_PRECISION
     pub volume_24h: u64,
+    /// the volume intensity of long fills against AMM
     pub long_intensity_volume: u64,
+    /// the volume intensity of short fills against AMM
     pub short_intensity_volume: u64,
+    /// the blockchain unix timestamp at the time of the last trade
     pub last_trade_ts: i64,
+    /// estimate of standard deviation of the fill (mark) prices
     pub mark_std: u64,
+    /// estimate of standard deviation of the oracle price at each update
     pub oracle_std: u64,
+    /// the last unix_timestamp the mark twap was updated
     pub last_mark_price_twap_ts: i64,
+    /// the minimum spread the AMM can quote. also used as step size for some spread logic increases.
     pub base_spread: u32,
+    /// the maximum spread the AMM can quote
     pub max_spread: u32,
+    /// the spread for asks vs the reserve price
     pub long_spread: u32,
+    /// the spread for bids vs the reserve price
     pub short_spread: u32,
+    /// the count intensity of long fills against AMM
     pub long_intensity_count: u32,
+    /// the count intensity of short fills against AMM
     pub short_intensity_count: u32,
+    /// the fraction of total available liquidity a single fill on the AMM can consume
     pub max_fill_reserve_fraction: u16,
+    /// the maximum slippage a single fill on the AMM can push
     pub max_slippage_ratio: u16,
+    /// the update intensity of AMM formulaic updates (adjusting k). 0-100
     pub curve_update_intensity: u8,
+    /// the jit intensity of AMM. larger intensity means larger participation in jit. 0 means no jit participation.
     pub amm_jit_intensity: u8,
+    /// the oracle provider information. used to decode/scale the oracle public key
     pub oracle_source: OracleSource,
+    /// tracks whether the oracle was considered valid at the last AMM update
     pub last_oracle_valid: bool,
+
     pub padding: [u8; 48],
 }
 
