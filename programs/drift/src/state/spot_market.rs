9c7837ca1ab7f3131d380f154e9d769b49c87aa1
Chris Heaney <chrisheaney30@gmail.com>
2023-05-04 12:18:04 -0500

    spot_market.rs comments

--- programs/drift/src/state/spot_market.rs
+++ programs/drift/src/state/spot_market.rs
@@ -24,52 +24,136 @@ use crate::state::traits::{MarketIndexOffset, Size};
 #[derive(PartialEq, Eq, Debug)]
 #[repr(C)]
 pub struct SpotMarket {
+    /// The address of the spot market. It is a pda of the market index
     pub pubkey: Pubkey,
+    /// The oracle used to price the markets deposits/borrows
     pub oracle: Pubkey,
+    /// The token mint of the market
     pub mint: Pubkey,
+    /// The vault used to store the market's deposits
+    /// The amount in the vault should be equal to or greater than deposits - borrows
     pub vault: Pubkey,
-    pub name: [u8; 32], // 256 bits
+    /// The encoded display name fo the market e.g. SOL
+    pub name: [u8; 32],
     pub historical_oracle_data: HistoricalOracleData,
     pub historical_index_data: HistoricalIndexData,
-    pub revenue_pool: PoolBalance,  // in base asset
-    pub spot_fee_pool: PoolBalance, // in quote asset
+    /// Revenue the protocol has collected in this markets token
+    /// e.g. for SOL-PERP, funds can be settled in usdc and will flow into the USDC revenue pool
+    pub revenue_pool: PoolBalance, // in base asset
+    /// The fees collected from swaps between this market and the quote market
+    /// Is settled to the quote markets revenue pool
+    pub spot_fee_pool: PoolBalance,
+    /// Details on the insurance fund covering bankruptcies in this markets token
+    /// Covers bankruptcies for borrows with this markets token and perps settling in this markets token
     pub insurance_fund: InsuranceFund,
+    /// The total spot fees collected for this market
+    /// precision: QUOTE_PRECISION
     pub total_spot_fee: u128,
+    /// The sum of the scaled balances for deposits across users and pool balances
+    /// To convert to the deposit token amount, multiply by the cumulative deposit interest
+    /// precision: SPOT_BALANCE_PRECISION
     pub deposit_balance: u128,
+    /// The sum of the scaled balances for borrows across users and pool balances
+    /// To convert to the borrow token amount, multiply by the cumulative borrow interest
+    /// precision: SPOT_BALANCE_PRECISION   
     pub borrow_balance: u128,
+    /// The cumulative interest earned by depositors
+    /// Used to calculate the deposit token amount from the deposit balance
+    /// precision: SPOT_CUMULATIVE_INTEREST_PRECISION
     pub cumulative_deposit_interest: u128,
+    /// The cumulative interest earned by borrowers
+    /// Used to calculate the borrow token amount from the borrow balance
+    /// precision: SPOT_CUMULATIVE_INTEREST_PRECISION
     pub cumulative_borrow_interest: u128,
+    /// The total socialized loss from borrows, in the mint's token
+    /// precision: token mint precision
     pub total_social_loss: u128,
+    /// The total socialized loss from borrows, in the quote market's token
+    /// preicision: QUOTE_PRECISION
     pub total_quote_social_loss: u128,
-    pub withdraw_guard_threshold: u64, // no withdraw limits/guards when deposits below this threshold
+    /// no withdraw limits/guards when deposits below this threshold
+    /// precision: token mint precision
+    pub withdraw_guard_threshold: u64,
+    /// The max amount of token deposits in this market
+    /// 0 if there is no limit
+    /// precision: token mint precision
     pub max_token_deposits: u64,
-    pub deposit_token_twap: u64, // 24 hour twap
-    pub borrow_token_twap: u64,  // 24 hour twap
-    pub utilization_twap: u64,   // 24 hour twap
+    /// 24hr average of deposit token amount
+    /// precision: token mint precision
+    pub deposit_token_twap: u64,
+    /// 24hr average of borrow token amount
+    /// precision: token mint precision
+    pub borrow_token_twap: u64,
+    /// 24hr average of utilization
+    /// which is borrow amount over token amount
+    /// precision: SPOT_UTILIZATION_PRECISION
+    pub utilization_twap: u64,
+    /// Last time the cumulative deposit and borrow interest was updated
     pub last_interest_ts: u64,
+    /// Last time the deposit/borrow/utilization averages were updated
     pub last_twap_ts: u64,
-    pub expiry_ts: i64, // iff market in reduce only mode
+    /// The time the market is set to expire. Only set if market is in reduce only mode
+    pub expiry_ts: i64,
+    /// Spot orders must be a multiple of the step size
+    /// precision: token mint precision
     pub order_step_size: u64,
+    /// Spot orders must be a multiple of the tick size
+    /// precision: QUOTE_PRECISION
     pub order_tick_size: u64,
+    /// The minimum order size
+    /// precision: token mint precision
     pub min_order_size: u64,
+    /// The maximum spot position size
+    /// if the limit is 0, there is no limit
+    /// precision: token mint precision
     pub max_position_size: u64,
+    /// Every spot trade has a fill record id. This is the next id to use
     pub next_fill_record_id: u64,
+    /// Every deposit has a deposit record id. This is the next id to use
     pub next_deposit_record_id: u64,
+    /// The initial asset weight used to calculate a deposits contribution to a users initial total collateral
+    /// e.g. if the asset weight is .8, $100 of deposits contributes $80 to the users initial total collateral
+    /// precision: SPOT_WEIGHT_PRECISION
     pub initial_asset_weight: u32,
+    /// The maintenance asset weight used to calculate a deposits contribution to a users maintenance total collateral
+    /// e.g. if the asset weight is .9, $100 of deposits contributes $90 to the users maintenance total collateral
+    /// precision: SPOT_WEIGHT_PRECISION
     pub maintenance_asset_weight: u32,
+    /// The initial liability weight used to calculate a borrows contribution to a users initial margin requirement
+    /// e.g. if the liability weight is .9, $100 of borrows contributes $90 to the users initial margin requirement
+    /// precision: SPOT_WEIGHT_PRECISION
     pub initial_liability_weight: u32,
+    /// The maintenance liability weight used to calculate a borrows contribution to a users maintenance margin requirement
+    /// e.g. if the liability weight is .8, $100 of borrows contributes $80 to the users maintenance margin requirement
+    /// precision: SPOT_WEIGHT_PRECISION
     pub maintenance_liability_weight: u32,
+    /// The initial margin fraction factor. Used to increase liability weight/decrease asset weight for large positions
+    /// precision: MARGIN_PRECISION
     pub imf_factor: u32,
+    /// The fee the liquidator is paid for taking over borrow/deposit
+    /// precision: LIQUIDATOR_FEE_PRECISION
     pub liquidator_fee: u32,
-    pub if_liquidation_fee: u32, // percentage of liquidation transfer for total insurance
-    pub optimal_utilization: u32, //
+    /// The fee the insurance fund receives from liquidation
+    /// precision: LIQUIDATOR_FEE_PRECISION
+    pub if_liquidation_fee: u32,
+    /// The optimal utilization rate for this market.
+    /// Used to determine the markets borrow rate
+    /// precision: SPOT_UTILIZATION_PRECISION
+    pub optimal_utilization: u32,
+    /// The borrow rate for this market when the market has optimal utilization
+    /// precision: SPOT_RATE_PRECISION
     pub optimal_borrow_rate: u32,
+    /// The borrow rate for this market when the market has 1000 utilization
+    /// precision: SPOT_RATE_PRECISION
     pub max_borrow_rate: u32,
+    /// The market's token mint's decimals. To from decimals to a precision, 10^decimals
     pub decimals: u32,
     pub market_index: u16,
+    /// Whether or not spot trading is enabled
     pub orders_enabled: bool,
     pub oracle_source: OracleSource,
     pub status: MarketStatus,
+    /// The asset tier affects how a deposit can be used as collateral and the priority for a borrow being liquidated
     pub asset_tier: AssetTier,
     pub padding: [u8; 86],
 }
@@ -355,11 +439,16 @@ impl Default for SpotFulfillmentConfigStatus {
 
 #[derive(Clone, Copy, BorshSerialize, BorshDeserialize, PartialEq, Debug, Eq, PartialOrd, Ord)]
 pub enum AssetTier {
-    Collateral, // full priviledge
-    Protected,  // collateral, but no borrow
-    Cross,      // not collateral, allow multi-borrow
-    Isolated,   // not collateral, only single borrow
-    Unlisted,   // no priviledge
+    /// full priviledge
+    Collateral,
+    /// collateral, but no borrow
+    Protected,
+    /// not collateral, allow multi-borrow
+    Cross,
+    /// not collateral, only single borrow
+    Isolated,
+    /// no privilege
+    Unlisted,
 }
 
 impl Default for AssetTier {
