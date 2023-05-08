61b116ba9eed312ac65c285bed392427d7f28686
0xbigz <83473873+0xbigz@users.noreply.github.com>
2023-05-08 12:17:28 -0400

    bigz/add-timeRemainingUntilUpdate-to-sdk

--- sdk/src/math/utils.ts
+++ sdk/src/math/utils.ts
@@ -33,3 +33,50 @@ export const divCeil = (a: BN, b: BN): BN => {
 		return quotient;
 	}
 };
+
+/**
+ * calculates the time remaining until the next update based on a rounded, "on-the-hour" update schedule
+ * this schedule is used for Perpetual Funding Rate and Revenue -> Insurance Updates
+ * @param now: current blockchain unix timestamp
+ * @param lastUpdateTs: the unix timestamp of the last update
+ * @param updatePeriod: desired interval between updates (in seconds)
+ * @returns: timeRemainingUntilUpdate (in seconds)
+ */
+export function timeRemainingUntilUpdate(
+	now: BN,
+	lastUpdateTs: BN,
+	updatePeriod: BN
+): BN {
+	const timeSinceLastUpdate = now.sub(lastUpdateTs);
+
+	// round next update time to be available on the hour
+	let nextUpdateWait = updatePeriod;
+	if (updatePeriod.gt(new BN(1))) {
+		const lastUpdateDelay = lastUpdateTs.umod(updatePeriod);
+		if (!lastUpdateDelay.isZero()) {
+			const maxDelayForNextPeriod = updatePeriod.div(new BN(3));
+
+			const twoFundingPeriods = updatePeriod.mul(new BN(2));
+
+			if (lastUpdateDelay.gt(maxDelayForNextPeriod)) {
+				// too late for on the hour next period, delay to following period
+				nextUpdateWait = twoFundingPeriods.sub(lastUpdateDelay);
+			} else {
+				// allow update on the hour
+				nextUpdateWait = updatePeriod.sub(lastUpdateDelay);
+			}
+
+			if (nextUpdateWait.gt(twoFundingPeriods)) {
+				nextUpdateWait = nextUpdateWait.sub(updatePeriod);
+			}
+		}
+	}
+
+	const timeRemainingUntilUpdate = nextUpdateWait
+		.sub(timeSinceLastUpdate)
+		.isNeg()
+		? new BN(0)
+		: nextUpdateWait.sub(timeSinceLastUpdate);
+
+	return timeRemainingUntilUpdate;
+}
