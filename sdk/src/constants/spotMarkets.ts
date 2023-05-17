f3421bb19dc3b9bd6d8cd32de6e3dd44533107ba
0xbigz <zane@lunoho.company>
2023-05-17 19:15:10 -0400

    init msol config

--- sdk/src/constants/spotMarkets.ts
+++ sdk/src/constants/spotMarkets.ts
@@ -57,6 +57,16 @@ export const DevnetSpotMarkets: SpotMarketConfig[] = [
 		precisionExp: SIX,
 		serumMarket: new PublicKey('AGsmbVu3MS9u68GEYABWosQQCZwmLcBHu4pWEuBYH7Za'),
 	},
+	{
+		symbol: 'mSOL',
+		marketIndex: 3,
+		oracle: new PublicKey('9a6RNx3tCu1TSs6TBSfV2XRXEPEZXQ6WB7jRojZRvyeZ'),
+		oracleSource: OracleSource.PYTH,
+		mint: new PublicKey('mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'),
+		precision: new BN(10).pow(SIX),
+		precisionExp: SIX,
+		// serumMarket: new PublicKey('AGsmbVu3MS9u68GEYABWosQQCZwmLcBHu4pWEuBYH7Za'), TODO
+	},
 ];
 
 export const MainnetSpotMarkets: SpotMarketConfig[] = [
@@ -81,6 +91,16 @@ export const MainnetSpotMarkets: SpotMarketConfig[] = [
 		phoenixMarket: new PublicKey(
 			'4DoNfFBfF7UokCC2FQzriy7yHK6DY6NVdYpuekQ5pRgg'
 		),
+	},
+	{
+		symbol: 'mSOL',
+		marketIndex: 2,
+		oracle: new PublicKey('E4v1BBgoso9s64TQvmyownAVJbhbEPGyzA3qn4n46qj9'),
+		oracleSource: OracleSource.PYTH,
+		mint: new PublicKey('mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So'),
+		precision: new BN(10).pow(SIX),
+		precisionExp: SIX,
+		serumMarket: new PublicKey('9Lyhks5bQQxb9EyyX55NtgKQzpM4WK7JCmeaWuQ5MoXD'),
 	},
 ];
 
