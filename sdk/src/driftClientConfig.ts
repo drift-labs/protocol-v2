ee7f9ce4845332a155bce915b14e93d88c122d15
lil perp <chrisheaney30@gmail.com>
2023-06-13 14:10:37 -0400

    sdk: allow default txParams on driftClient (#496)
    * sdk: allow default txParams on driftClient
    
    * CHANGELOG

--- sdk/src/driftClientConfig.ts
+++ sdk/src/driftClientConfig.ts
@@ -4,7 +4,7 @@ import {
 	PublicKey,
 	TransactionVersion,
 } from '@solana/web3.js';
-import { IWallet } from './types';
+import { IWallet, TxParams } from './types';
 import { OracleInfo } from './oracles/types';
 import { BulkAccountLoader } from './accounts/bulkAccountLoader';
 import { DriftEnv } from './config';
@@ -30,6 +30,7 @@ export type DriftClientConfig = {
 	authoritySubAccountMap?: Map<string, number[]>; // if passed this will override subAccountIds and includeDelegates
 	skipLoadUsers?: boolean; // if passed to constructor, no user accounts will be loaded. they will load if updateWallet is called afterwards.
 	txVersion?: TransactionVersion; // which tx version to use
+	txParams?: TxParams; // default tx params to use
 };
 
 export type DriftClientSubscriptionConfig =
