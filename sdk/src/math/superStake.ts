45fb572af6bce28031d3309190d8c9e97d1dd027
Chris Heaney <chrisheaney30@gmail.com>
2023-06-21 15:54:15 -0400

    sdk: add calculateSolEarned

--- sdk/src/math/superStake.ts
+++ sdk/src/math/superStake.ts
@@ -1,5 +1,6 @@
 import {
 	AddressLookupTableAccount,
+	LAMPORTS_PER_SOL,
 	PublicKey,
 	TransactionInstruction,
 } from '@solana/web3.js';
@@ -7,6 +8,9 @@ import { JupiterClient } from '../jupiter/jupiterClient';
 import { DriftClient } from '../driftClient';
 import { getMarinadeFinanceProgram, getMarinadeMSolPrice } from '../marinade';
 import { BN } from '@coral-xyz/anchor';
+import { User } from '../user';
+import { DepositRecord, isVariant } from '../types';
+import { LAMPORTS_PRECISION, ZERO } from '../constants/numericConstants';
 
 export async function findBestSuperStakeIxs({
 	amount,
@@ -62,4 +66,63 @@ export async function findBestSuperStakeIxs({
 			price: jupiterPrice,
 		};
 	}
+}
+
+export async function calculateSolEarned({
+	user,
+	depositRecords,
+}: {
+	user: User;
+	depositRecords: DepositRecord[];
+}): Promise<BN> {
+	const now = Date.now() / 1000;
+	const timestamps: number[] = [
+		now,
+		...depositRecords.map((r) => r.ts.toNumber()),
+	];
+
+	const msolRatios = new Map<number, number>();
+
+	const getPrice = async (timestamp) => {
+		const date = new Date(timestamp * 1000); // Convert Unix timestamp to milliseconds
+		const swaggerApiDateTime = date.toISOString(); // Format date as swagger API date-time
+		const url = `https://api.marinade.finance/msol/price_sol?time=${swaggerApiDateTime}`;
+		const response = await fetch(url);
+		if (response.status === 200) {
+			const data = await response.json();
+			msolRatios.set(timestamp, data);
+		}
+	};
+
+	await Promise.all(timestamps.map(getPrice));
+
+	let solEarned = ZERO;
+	for (const record of depositRecords) {
+		if (record.marketIndex !== 2) {
+			continue;
+		}
+
+		const msolRatio = msolRatios.get(record.ts.toNumber());
+		const msolRatioBN = new BN(msolRatio * LAMPORTS_PER_SOL);
+
+		const solAmount = record.amount.mul(msolRatioBN).div(LAMPORTS_PRECISION);
+		if (isVariant(record.explanation, 'deposit')) {
+			solEarned = solEarned.sub(solAmount);
+		} else {
+			solEarned = solEarned.add(solAmount);
+		}
+	}
+
+	const currentMSOLTokenAmount = await user.getTokenAmount(2);
+	const currentSOLTokenAmount = await user.getTokenAmount(1);
+
+	const currentMSOLRatio = msolRatios.get(now);
+	const currentMSOLRatioBN = new BN(currentMSOLRatio * LAMPORTS_PER_SOL);
+
+	solEarned = solEarned.add(
+		currentMSOLTokenAmount.mul(currentMSOLRatioBN).div(LAMPORTS_PRECISION)
+	);
+	solEarned = solEarned.add(currentSOLTokenAmount);
+
+	return solEarned;
 }
