e8696a180fdc211bb55f0127124ce3a8068e5ea8
Chris Heaney <chrisheaney30@gmail.com>
2023-06-24 18:03:45 -0400

    sdk: add memcmp filter for user name

--- sdk/src/memcmp.ts
+++ sdk/src/memcmp.ts
@@ -1,6 +1,7 @@
 import { MemcmpFilter } from '@solana/web3.js';
 import bs58 from 'bs58';
 import { BorshAccountsCoder } from '@coral-xyz/anchor';
+import { encodeName } from './userName';
 
 export function getUserFilter(): MemcmpFilter {
 	return {
@@ -43,6 +44,15 @@ export function getUserThatHasBeenLP(): MemcmpFilter {
 		memcmp: {
 			offset: 4267,
 			bytes: bs58.encode(Uint8Array.from([99])),
+		},
+	};
+}
+
+export function getUserWithName(name: string): MemcmpFilter {
+	return {
+		memcmp: {
+			offset: 72,
+			bytes: bs58.encode(Uint8Array.from(encodeName(name))),
 		},
 	};
 }
