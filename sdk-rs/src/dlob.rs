4676b42575bd880bdefbcb6e886b53d89958c9b4
wphan <william@drift.trade>
2023-07-04 11:36:34 -0700

    fix update_resting_limit_order insert order

--- sdk-rs/src/dlob.rs
+++ sdk-rs/src/dlob.rs
@@ -26,7 +26,7 @@ pub enum DlobNodeType {
     Trigger,
 }
 
-pub trait DlobNode {
+trait DlobNode: std::fmt::Debug {
     fn get_price(
         &self,
         oracle_price_data: OraclePriceData,
@@ -41,7 +41,7 @@ pub trait DlobNode {
     // fn get_node_type(&self) -> DlobNodeType;
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct TakingLimitOrderNode {
     user: Pubkey,
     order: Order,
@@ -78,7 +78,7 @@ impl DlobNode for TakingLimitOrderNode {
     // }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct RestingLimitOrderNode {
     user: Pubkey,
     order: Order,
@@ -115,7 +115,7 @@ impl DlobNode for RestingLimitOrderNode {
     // }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct FloatingLimitOrderNode {
     user: Pubkey,
     order: Order,
@@ -152,7 +152,7 @@ impl DlobNode for FloatingLimitOrderNode {
     // }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct MarketOrderNode {
     user: Pubkey,
     order: Order,
@@ -189,7 +189,7 @@ impl DlobNode for MarketOrderNode {
     // }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct TriggerOrderNode {
     user: Pubkey,
     order: Order,
@@ -582,24 +582,40 @@ impl Dlob {
 
         self.max_slot_for_resting_limit_orders = slot;
 
-        self.perp_order_lists.iter_mut().for_each(|(_, list)| {
-            list.taking_limit.bid.retain(|node| {
-                if !node.order().is_resting_limit_order(slot).unwrap() {
-                    list.resting_limit.bid.push(node.clone());
-                    return true;
-                } else {
-                    return false;
-                }
+        let mut new_resting_orders: Vec<Rc<dyn DlobNode>> = vec![];
+        for market_type in vec![MarketType::Perp, MarketType::Spot] {
+            let order_list = match market_type {
+                MarketType::Perp => &mut self.perp_order_lists,
+                MarketType::Spot => &mut self.spot_order_lists,
+            };
+            order_list.iter_mut().for_each(|(_, list)| {
+                list.taking_limit.bid.retain(|node| {
+                    if !node.order().is_resting_limit_order(slot).unwrap() {
+                        return true;
+                    } else {
+                        new_resting_orders.push(node.clone());
+                        return false;
+                    }
+                });
+                list.taking_limit.ask.retain(|node| {
+                    if !node.order().is_resting_limit_order(slot).unwrap() {
+                        return true;
+                    } else {
+                        new_resting_orders.push(node.clone());
+                        return false;
+                    }
+                });
             });
-            list.taking_limit.ask.retain(|node| {
-                if !node.order().is_resting_limit_order(slot).unwrap() {
-                    list.resting_limit.ask.push(node.clone());
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-        });
+        }
+
+        for node in new_resting_orders {
+            self.insert_order_by_type::<RestingLimitOrderNode>(
+                DlobNodeType::RestingLimit,
+                node.user(),
+                node.order(),
+            )
+            .unwrap();
+        }
     }
 
     pub fn get_taking_bids(
@@ -1111,6 +1127,246 @@ mod tests {
                         assert!(got == want, "got: {}, want: {}", got, want);
                     }
                 }
+            }
+        }
+    }
+
+    #[test]
+    fn test_update_resting_limit_orders() {
+        struct TestData {
+            order_id: u32,
+            direction: PositionDirection,
+            price: u64,
+            auction_duration: u8,
+            slot: u64,
+        }
+
+        let market_index = 0;
+        for market_type in vec![MarketType::Perp, MarketType::Spot] {
+            let mut dlob = DlobBuilder::default()
+                .account_subscriber(Box::new(MockDriftClientAccountSubscriber::default()))
+                .build()
+                .unwrap();
+            let user = Pubkey::new_unique();
+            dlob.load().unwrap();
+            dlob.ensure_market_index_in_list(market_type, market_index);
+
+            for data in vec![
+                TestData {
+                    order_id: 1,
+                    direction: PositionDirection::Long,
+                    price: (1.31 * PRICE_PRECISION_U64 as f64) as u64, // resting bid idx 1
+                    auction_duration: 10,
+                    slot: 1,
+                },
+                TestData {
+                    order_id: 2,
+                    direction: PositionDirection::Long,
+                    price: (1.91 * PRICE_PRECISION_U64 as f64) as u64, // resting bid idx 0
+                    auction_duration: 10,
+                    slot: 2,
+                },
+                TestData {
+                    order_id: 3,
+                    direction: PositionDirection::Long,
+                    price: (0.23 * PRICE_PRECISION_U64 as f64) as u64, // resting bid idx 2
+                    auction_duration: 10,
+                    slot: 3,
+                },
+                TestData {
+                    order_id: 4,
+                    direction: PositionDirection::Short,
+                    price: (1.01 * PRICE_PRECISION_U64 as f64) as u64, // resting ask idx 0
+                    auction_duration: 10,
+                    slot: 4,
+                },
+                TestData {
+                    order_id: 5,
+                    direction: PositionDirection::Short,
+                    price: (2.22 * PRICE_PRECISION_U64 as f64) as u64, // resting ask idx 2
+                    auction_duration: 10,
+                    slot: 5,
+                },
+                TestData {
+                    order_id: 6,
+                    direction: PositionDirection::Short,
+                    price: (1.35 * PRICE_PRECISION_U64 as f64) as u64, // resting ask idx 1
+                    auction_duration: 10,
+                    slot: 6,
+                },
+            ] {
+                let mut order = Order::default();
+                order.status = OrderStatus::Open;
+                order.market_index = market_index;
+                order.market_type = market_type;
+                order.order_id = data.order_id;
+                order.direction = data.direction;
+                order.order_type = OrderType::Limit;
+                order.price = data.price;
+                order.auction_duration = data.auction_duration;
+                order.slot = data.slot;
+                dlob.insert_order(6, user, order).unwrap();
+            }
+
+            // initially all orders are taking (auctions in progress)
+            // initially all taking
+            dlob.update_resting_limit_orders(6);
+            {
+                let orders_list = match market_type {
+                    MarketType::Perp => dlob.perp_order_lists.get(&market_index).unwrap(),
+                    MarketType::Spot => dlob.spot_order_lists.get(&market_index).unwrap(),
+                };
+                assert_eq!(
+                    orders_list.taking_limit.bid.len(),
+                    3,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.taking_limit.ask.len(),
+                    3,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+            }
+
+            // first bid auction ends, first order is resting
+            dlob.update_resting_limit_orders(12);
+            {
+                let orders_list = match market_type {
+                    MarketType::Perp => dlob.perp_order_lists.get(&market_index).unwrap(),
+                    MarketType::Spot => dlob.spot_order_lists.get(&market_index).unwrap(),
+                };
+                assert_eq!(
+                    orders_list.taking_limit.bid.len(),
+                    2,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.taking_limit.ask.len(),
+                    3,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.bid.len(),
+                    1,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.ask.len(),
+                    0,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+            }
+
+            // next 2 bid auction ends, all bids are resting
+            dlob.update_resting_limit_orders(14);
+            {
+                let orders_list = match market_type {
+                    MarketType::Perp => dlob.perp_order_lists.get(&market_index).unwrap(),
+                    MarketType::Spot => dlob.spot_order_lists.get(&market_index).unwrap(),
+                };
+                assert_eq!(
+                    orders_list.taking_limit.bid.len(),
+                    0,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.taking_limit.ask.len(),
+                    3,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.bid.len(),
+                    3,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.ask.len(),
+                    0,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+
+                // verify that final resting bids are in the correct order
+                assert_eq!(
+                    orders_list.resting_limit.bid[0].order().order_id,
+                    2,
+                    "{:?} bids wrong order",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.bid[1].order().order_id,
+                    1,
+                    "{:?} bids wrong order",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.bid[2].order().order_id,
+                    3,
+                    "{:?} bids wrong order",
+                    market_type
+                );
+            }
+
+            // all ask auction ends, all asksare resting
+            dlob.update_resting_limit_orders(17);
+            {
+                let orders_list = match market_type {
+                    MarketType::Perp => dlob.perp_order_lists.get(&market_index).unwrap(),
+                    MarketType::Spot => dlob.spot_order_lists.get(&market_index).unwrap(),
+                };
+                assert_eq!(
+                    orders_list.taking_limit.bid.len(),
+                    0,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.taking_limit.ask.len(),
+                    0,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.bid.len(),
+                    3,
+                    "{:?} bids wrong length",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.ask.len(),
+                    3,
+                    "{:?} asks wrong length",
+                    market_type
+                );
+
+                // verify that final resting asks are in the correct order
+                assert_eq!(
+                    orders_list.resting_limit.ask[0].order().order_id,
+                    4,
+                    "{:?} asks wrong order",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.ask[1].order().order_id,
+                    6,
+                    "{:?} asks wrong order",
+                    market_type
+                );
+                assert_eq!(
+                    orders_list.resting_limit.ask[2].order().order_id,
+                    5,
+                    "{:?} asks wrong order",
+                    market_type
+                );
             }
         }
     }
--- sdk-rs/src/macros.rs
+++ sdk-rs/src/macros.rs
@@ -1,13 +1,1 @@
-#[macro_export]
-macro_rules! assert {
-    ($assert:expr) => {{
-        if !$assert {
-            panic!("assertion failed: {}", stringify!($assert));
-        }
-    }};
-    ($assert:expr, $($arg:tt)+) => {{
-        if !$assert {
-            panic!("assertion failed: {}: {}", stringify!($assert), format_args!($($arg)+));
-        }
-    }};
-}
+
