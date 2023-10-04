3488cd2911deeb91e02eae45f245bbe6088a7e8a
Luke Steyn <lukecaan@gmail.com>
2023-10-04 23:29:15 +0200

    removed debug logging and fixed bug in bulkAccountLoader

--- sdk/src/accounts/bulkAccountLoader.ts
+++ sdk/src/accounts/bulkAccountLoader.ts
@@ -177,12 +177,12 @@ export class BulkAccountLoader {
 			return;
 		}
 
-		for (const i in rpcResponses) {
-			const rpcResponse = rpcResponses[i];
+		// for (const i in rpcResponses) {
+		rpcResponses.forEach((rpcResponse, i) => {
 			if (!rpcResponse.result) {
 				console.error('rpc response missing result:');
 				console.log(JSON.stringify(rpcResponse));
-				continue;
+				return;
 			}
 			const newSlot = rpcResponse.result.context.slot;
 
@@ -191,13 +191,12 @@ export class BulkAccountLoader {
 			}
 
 			const accountsToLoad = accountsToLoadChunks[i];
-			for (const j in accountsToLoad) {
-				const accountToLoad = accountsToLoad[j];
+			accountsToLoad.forEach((accountToLoad, j) => {
 				const key = accountToLoad.publicKey.toBase58();
 				const oldRPCResponse = this.bufferAndSlotMap.get(key);
 
 				if (oldRPCResponse && newSlot <= oldRPCResponse.slot) {
-					continue;
+					return;
 				}
 
 				let newBuffer: Buffer | undefined = undefined;
@@ -213,7 +212,7 @@ export class BulkAccountLoader {
 						buffer: newBuffer,
 					});
 					this.handleAccountCallbacks(accountToLoad, newBuffer, newSlot);
-					continue;
+					return;
 				}
 
 				const oldBuffer = oldRPCResponse.buffer;
@@ -224,8 +223,8 @@ export class BulkAccountLoader {
 					});
 					this.handleAccountCallbacks(accountToLoad, newBuffer, newSlot);
 				}
-			}
-		}
+			});
+		});
 	}
 
 	handleAccountCallbacks(
