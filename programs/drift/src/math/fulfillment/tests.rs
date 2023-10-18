a3719a3286ce7fe80e0581ad1d9bf375a224e6a2
Chris Heaney <chrisheaney30@gmail.com>
2023-10-18 17:53:12 -0400

    make determine_perp_fulfillment_methods clearer for amm taking against maker

--- programs/drift/src/math/fulfillment/tests.rs
+++ programs/drift/src/math/fulfillment/tests.rs
@@ -765,4 +765,126 @@ mod determine_perp_fulfillment_methods {
 
         assert_eq!(fulfillment_methods, []);
     }
+
+    #[test]
+    fn amm_available_and_maker_crosses() {
+        let mut market = PerpMarket {
+            amm: AMM {
+                base_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                quote_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                bid_base_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                bid_quote_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                ask_base_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                ask_quote_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                sqrt_k: 100 * AMM_RESERVE_PRECISION,
+                peg_multiplier: 100 * PEG_PRECISION,
+                max_slippage_ratio: 50,
+                max_fill_reserve_fraction: 100,
+                order_step_size: 10000000,
+                order_tick_size: 1,
+                base_spread: 100,
+                historical_oracle_data: HistoricalOracleData {
+                    last_oracle_price: (100 * PRICE_PRECISION) as i64,
+                    last_oracle_price_twap: (100 * PRICE_PRECISION) as i64,
+                    last_oracle_price_twap_5min: (100 * PRICE_PRECISION) as i64,
+
+                    ..HistoricalOracleData::default()
+                },
+                ..AMM::default()
+            },
+            margin_ratio_initial: 1000,
+            margin_ratio_maintenance: 500,
+            status: MarketStatus::Initialized,
+            ..PerpMarket::default_test()
+        };
+        market.amm.max_base_asset_reserve = u128::MAX;
+        market.amm.min_base_asset_reserve = 0;
+
+        let taker_order = Order {
+            post_only: true,
+            direction: PositionDirection::Long,
+            price: 102 * PRICE_PRECISION_U64,
+            ..Order::default()
+        };
+
+        let oracle_price = 100 * PRICE_PRECISION_I64;
+
+        let taker_price = Some(taker_order.price);
+
+        let fulfillment_methods = determine_perp_fulfillment_methods(
+            &taker_order,
+            &[],
+            &market.amm,
+            market.amm.reserve_price().unwrap(),
+            Some(oracle_price),
+            taker_price,
+            true,
+            0,
+            0,
+        )
+        .unwrap();
+
+        assert_eq!(fulfillment_methods, [PerpFulfillmentMethod::AMM(None)]);
+    }
+
+    #[test]
+    fn amm_available_and_maker_doesnt_cross() {
+        let mut market = PerpMarket {
+            amm: AMM {
+                base_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                quote_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                bid_base_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                bid_quote_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                ask_base_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                ask_quote_asset_reserve: 100 * AMM_RESERVE_PRECISION,
+                sqrt_k: 100 * AMM_RESERVE_PRECISION,
+                peg_multiplier: 100 * PEG_PRECISION,
+                max_slippage_ratio: 50,
+                max_fill_reserve_fraction: 100,
+                order_step_size: 10000000,
+                order_tick_size: 1,
+                base_spread: 100,
+                historical_oracle_data: HistoricalOracleData {
+                    last_oracle_price: (100 * PRICE_PRECISION) as i64,
+                    last_oracle_price_twap: (100 * PRICE_PRECISION) as i64,
+                    last_oracle_price_twap_5min: (100 * PRICE_PRECISION) as i64,
+
+                    ..HistoricalOracleData::default()
+                },
+                ..AMM::default()
+            },
+            margin_ratio_initial: 1000,
+            margin_ratio_maintenance: 500,
+            status: MarketStatus::Initialized,
+            ..PerpMarket::default_test()
+        };
+        market.amm.max_base_asset_reserve = u128::MAX;
+        market.amm.min_base_asset_reserve = 0;
+
+        let taker_order = Order {
+            post_only: true,
+            direction: PositionDirection::Long,
+            price: 99 * PRICE_PRECISION_U64,
+            ..Order::default()
+        };
+
+        let oracle_price = 100 * PRICE_PRECISION_I64;
+
+        let taker_price = Some(taker_order.price);
+
+        let fulfillment_methods = determine_perp_fulfillment_methods(
+            &taker_order,
+            &[],
+            &market.amm,
+            market.amm.reserve_price().unwrap(),
+            Some(oracle_price),
+            taker_price,
+            true,
+            0,
+            0,
+        )
+        .unwrap();
+
+        assert_eq!(fulfillment_methods, vec![]);
+    }
 }
