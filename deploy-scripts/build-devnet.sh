95af3e9f568b7eb42b2a07772b936c39c371c123
Chris Heaney <chrisheaney30@gmail.com>
2023-12-04 17:53:05 -0500

    add devnet deploy scripts

---
+++ deploy-scripts/build-devnet.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+anchor build -- --no-default-features
---
+++ deploy-scripts/deploy-devnet.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+anchor upgrade --program-id dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH --provider.cluster devnet --provider.wallet $SOLANA_PATH/$DEVNET_ADMIN target/deploy/drift.so
