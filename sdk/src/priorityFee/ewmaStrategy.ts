4ec29f324cac948bdf8a0770a9c9e626c09902f5
wphan <william@drift.trade>
2023-12-18 13:57:26 -0800

    sdk: add custom priority fee strategies (#779)
    * sdk: add custom priority fee strategies
    
    * add max and avg over slots strategies
    
    * sort getRecentPrioritizationFee response
    
    * prettify
    
    * fix ewma and strategy comments

---
+++ sdk/src/priorityFee/ewmaStrategy.ts
@@ -0,0 +1,40 @@
+import { PriorityFeeStrategy } from './types';
+
+class EwmaStrategy implements PriorityFeeStrategy {
+	private halfLife: number;
+
+	/**
+	 * @param halfLife The half life of the EWMA in slots. Default is 25 slots, approx 10 seconds.
+	 */
+	constructor(halfLife = 25) {
+		this.halfLife = halfLife;
+	}
+
+	// samples provided in desc slot order
+	calculate(samples: { slot: number; prioritizationFee: number }[]): number {
+		if (samples.length === 0) {
+			return 0;
+		}
+		if (samples.length === 1) {
+			return samples[0].prioritizationFee;
+		}
+
+		let ewma = 0;
+
+		const samplesReversed = samples.slice().reverse();
+		for (let i = 0; i < samplesReversed.length; i++) {
+			if (i === 0) {
+				ewma = samplesReversed[i].prioritizationFee;
+				continue;
+			}
+			const gap = samplesReversed[i].slot - samplesReversed[i - 1].slot;
+			const alpha = 1 - Math.exp((Math.log(0.5) / this.halfLife) * gap);
+
+			ewma = alpha * samplesReversed[i].prioritizationFee + (1 - alpha) * ewma;
+		}
+
+		return ewma;
+	}
+}
+
+export { EwmaStrategy };
