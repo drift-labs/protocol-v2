114fa014737995c249e0834f56ba3ff528ddbdbb
frank <98238480+soundsonacid@users.noreply.github.com>
2023-12-21 20:08:37 -0600

    feat: get_l3 and stream_l3 to rust sdk (#1)

--- sdk-rs/src/dlob.rs
+++ sdk-rs/src/dlob.rs
@@ -11,7 +11,9 @@ use tokio::sync::mpsc::{channel, Receiver};
 
 use crate::types::{MarketId, SdkError};
 
-pub type OrderbookStream = RxStream<Result<L2Orderbook, SdkError>>;
+pub type L2OrderbookStream = RxStream<Result<L2Orderbook, SdkError>>;
+
+pub type L3OrderbookStream = RxStream<Result<L3Orderbook, SdkError>>;
 
 #[derive(Clone)]
 /// Decentralized limit orderbook client
@@ -45,8 +47,26 @@ impl DLOBClient {
         let body = response.bytes().await?;
         serde_json::from_slice(body.as_ref()).map_err(|_| SdkError::Deserializing)
     }
-    /// Subscribe to a DLOB for `market`
-    pub fn subscribe(&self, market: MarketId, interval_s: Option<u64>) -> OrderbookStream {
+
+    pub async fn get_l3(&self, market: MarketId) -> Result<L3Orderbook, SdkError> {
+        let market_type = match market.kind {
+            MarketType::Perp => "perp",
+            MarketType::Spot => "spot",
+        };
+        let response = self
+            .client
+            .get(format!(
+                "{}/l3?marketType={}&marketIndex={}",
+                &self.url, market_type, market.index
+            ))
+            .send()
+            .await?;
+        let body = response.bytes().await?;
+        serde_json::from_slice(body.as_ref()).map_err(|_| SdkError::Deserializing)
+    }
+
+    /// Subscribe to a DLOB L2 book for `market`
+    pub fn subscribe_l2_book(&self, market: MarketId, interval_s: Option<u64>) -> L2OrderbookStream {
         let mut interval = tokio::time::interval(Duration::from_secs(interval_s.unwrap_or(1)));
         let (tx, rx) = channel(16);
         tokio::spawn({
@@ -55,6 +75,26 @@ impl DLOBClient {
                 loop {
                     let _ = interval.tick().await;
                     if tx.try_send(client.get_l2(market).await).is_err() {
+                        // capacity reached or receiver closed, end the subscription task
+                        break;
+                    }
+                }
+            }
+        });
+
+        RxStream(rx)
+    }
+
+    // Subscribe to a DLOB L3 book for `market`
+    pub fn subscribe_l3_book(&self, market: MarketId, interval_s: Option<u64>) -> L3OrderbookStream {
+        let mut interval = tokio::time::interval(Duration::from_secs(interval_s.unwrap_or(1)));
+        let (tx, rx) = channel(16);
+        tokio::spawn( {
+            let client = self.clone();
+            async move {
+                loop {
+                    let _ = interval.tick().await;
+                    if tx.try_send(client.get_l3(market).await).is_err() {
                         // capacity reached or receiver closed, end the subscription task
                         break;
                     }
@@ -95,11 +135,31 @@ pub struct L2Orderbook {
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct L3Orderbook {
+    /// sorted bids, highest first
+    pub bids: Vec<L3Level>,
+    /// sorted asks, lowest first
+    pub asks: Vec<L3Level>,
+    pub slot: u64
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct L2Level {
     #[serde(deserialize_with = "parse_int_str")]
     pub price: u64,
     #[serde(deserialize_with = "parse_int_str")]
     pub size: u64,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct L3Level {
+    #[serde(deserialize_with = "parse_int_str")]
+    pub price: u64,
+    #[serde(deserialize_with = "parse_int_str")]
+    pub size: u64,
+    pub maker: String,
+    #[serde(rename = "orderId")]
+    pub order_id: u64
 }
 
 fn parse_int_str<'de, D>(deserializer: D) -> Result<u64, D::Error>
@@ -130,7 +190,28 @@ mod tests {
     async fn stream_l2_book() {
         let url = "https://dlob.drift.trade";
         let client = DLOBClient::new(url);
-        let stream = client.subscribe(MarketId::perp(0), None);
+        let stream = client.subscribe_l2_book(MarketId::perp(0), None);
+        let mut short_stream = stream.take(5);
+        while let Some(book) = short_stream.next().await {
+            dbg!(book);
+        }
+    }
+
+    #[tokio::test]
+    async fn pull_l3_book() {
+        let url = "https://dlob.drift.trade";
+        let client = DLOBClient::new(url);
+        let perp_book = client.get_l3(MarketId::perp(0)).await.unwrap();
+        dbg!(perp_book);
+        let spot_book = client.get_l3(MarketId::spot(2)).await.unwrap();
+        dbg!(spot_book);
+    }
+
+    #[tokio::test]
+    async fn stream_l3_book() {
+        let url = "https://dlob.drift.trade";
+        let client = DLOBClient::new(url);
+        let stream = client.subscribe_l3_book(MarketId::perp(0), None);
         let mut short_stream = stream.take(5);
         while let Some(book) = short_stream.next().await {
             dbg!(book);
