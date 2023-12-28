bc4afed2199332bed609218a287a22f54032a4a6
lowkeynicc <85139158+lowkeynicc@users.noreply.github.com>
2023-12-28 11:22:22 -0500

    sdk: add txparams to withdraw (#799)
    * sdk: add txparams to withdraw
    
    * update changelog

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -2049,7 +2049,8 @@ export class DriftClient {
 		marketIndex: number,
 		associatedTokenAddress: PublicKey,
 		reduceOnly = false,
-		subAccountId?: number
+		subAccountId?: number,
+		txParams?: TxParams
 	): Promise<TransactionSignature> {
 		const withdrawIxs = [];
 
@@ -2112,6 +2113,7 @@ export class DriftClient {
 		}
 
 		const tx = await this.buildTransaction(withdrawIxs, {
+			...(txParams ?? this.txParams),
 			computeUnits: 1_400_000,
 		});
 		const { txSig, slot } = await this.sendTransaction(
