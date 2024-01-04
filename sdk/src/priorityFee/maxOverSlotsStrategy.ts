1d80b21b0242fbe8ddd58e98f3cf5be0605b8dc9
Luke <lukecaan@gmail.com>
2024-01-04 11:15:44 +0100

    Updated slot subscriber provider (#798)
    * Updated slot subscriber provider api
    
    * Bump
    
    * bump
    
    * bump
    
    * Preittify
    
    * Bump
    
    * Bump

--- sdk/src/priorityFee/maxOverSlotsStrategy.ts
+++ sdk/src/priorityFee/maxOverSlotsStrategy.ts
@@ -1,30 +1,15 @@
 import { PriorityFeeStrategy } from './types';
 
 export class MaxOverSlotsStrategy implements PriorityFeeStrategy {
-	private lookbackSlots: number;
-
-	/**
-	 * @param lookbackSlots The number of slots to look back from the max slot in the sample
-	 */
-	constructor(lookbackSlots = 10) {
-		this.lookbackSlots = lookbackSlots;
-	}
-
 	calculate(samples: { slot: number; prioritizationFee: number }[]): number {
 		if (samples.length === 0) {
 			return 0;
 		}
 		// Assuming samples are sorted in descending order of slot.
-		const stopSlot = samples[0].slot - this.lookbackSlots;
 		let currMaxFee = samples[0].prioritizationFee;
 
 		for (let i = 0; i < samples.length; i++) {
-			if (samples[i].slot <= stopSlot) {
-				return currMaxFee;
-			}
-			if (samples[i].prioritizationFee > currMaxFee) {
-				currMaxFee = samples[i].prioritizationFee;
-			}
+			currMaxFee = Math.max(samples[i].prioritizationFee, currMaxFee);
 		}
 		return currMaxFee;
 	}
