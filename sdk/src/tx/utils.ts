ac2f5619eb4ffcf6f9a194a047467316400b6abc
lowkeynicc <85139158+lowkeynicc@users.noreply.github.com>
2024-01-09 13:52:53 -0500

    sdk: add option to get signed settlepnl tix from a market order (#813)
    * sdk: add option to get signed settlepnl tix from a market order
    
    * fix lint

--- sdk/src/tx/utils.ts
+++ sdk/src/tx/utils.ts
@@ -1,7 +1,9 @@
+import { Wallet } from '@coral-xyz/anchor';
 import {
 	Transaction,
 	TransactionInstruction,
 	ComputeBudgetProgram,
+	VersionedTransaction,
 } from '@solana/web3.js';
 
 const COMPUTE_UNITS_DEFAULT = 200_000;
@@ -29,4 +31,34 @@ export function wrapInTx(
 	}
 
 	return tx.add(instruction);
+}
+
+/* Helper function for signing multiple transactions where some may be undefined and mapping the output */
+export async function getSignedTransactionMap(
+	wallet: Wallet,
+	txsToSign: (Transaction | VersionedTransaction | undefined)[],
+	keys: string[]
+): Promise<{ [key: string]: Transaction | VersionedTransaction | undefined }> {
+	const signedTxMap: {
+		[key: string]: Transaction | VersionedTransaction | undefined;
+	} = {};
+
+	const keysWithTx = [];
+	txsToSign.forEach((tx, index) => {
+		if (tx == undefined) {
+			signedTxMap[keys[index]] = undefined;
+		} else {
+			keysWithTx.push(keys[index]);
+		}
+	});
+
+	const signedTxs = await wallet.signAllTransactions(
+		txsToSign.filter((tx) => tx !== undefined)
+	);
+
+	signedTxs.forEach((signedTx, index) => {
+		signedTxMap[keysWithTx[index]] = signedTx;
+	});
+
+	return signedTxMap;
 }
