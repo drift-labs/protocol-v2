3f3b5a1b4598b8f39ff3ce70c9bfc5a7c1e42c95
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-01-25 17:24:30 -0500

    devnet-market-index-23

--- sdk/src/constants/perpMarkets.ts
+++ sdk/src/constants/perpMarkets.ts
@@ -244,6 +244,16 @@ export const DevnetPerpMarkets: PerpMarketConfig[] = [
 		launchTs: 1704209558000,
 		oracleSource: OracleSource.PYTH,
 	},
+	{
+		fullName: 'WIF',
+		category: ['Meme', 'Dog'],
+		symbol: 'WIF-PERP',
+		baseAssetSymbol: 'WIF',
+		marketIndex: 23,
+		oracle: new PublicKey('5i1sz2QQjCQt9PnhuPvqbiYUAYCgjdRnza1JbiH2qRCo'),
+		launchTs: 1706219971000,
+		oracleSource: OracleSource.PYTH,
+	},
 ];
 
 export const MainnetPerpMarkets: PerpMarketConfig[] = [
@@ -475,6 +485,16 @@ export const MainnetPerpMarkets: PerpMarketConfig[] = [
 		marketIndex: 22,
 		oracle: new PublicKey('Ax9ujW5B9oqcv59N8m6f1BpTBq2rGeGaBcpKjC5UYsXU'),
 		launchTs: 1704209558000,
+		oracleSource: OracleSource.PYTH,
+	},
+	{
+		fullName: 'WIF',
+		category: ['Meme', 'Dog'],
+		symbol: 'WIF-PERP',
+		baseAssetSymbol: 'WIF',
+		marketIndex: 23,
+		oracle: new PublicKey('6ABgrEZk8urs6kJ1JNdC1sspH5zKXRqxy8sg3ZG2cQps'),
+		launchTs: 1706219971000,
 		oracleSource: OracleSource.PYTH,
 	},
 ];
