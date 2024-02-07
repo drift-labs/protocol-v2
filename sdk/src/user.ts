e3e4581c012481f9000da9df4c51fb817e0c08df
lowkeynicc <nick@lunoho.company>
2024-02-07 09:50:27 -0500

    sdk: move 30d vol estimate

--- sdk/src/user.ts
+++ sdk/src/user.ts
@@ -55,6 +55,7 @@ import {
 	getSignedTokenAmount,
 	getStrictTokenValue,
 	getTokenValue,
+	getUser30dRollingVolumeEstimate,
 	MarketType,
 	PositionDirection,
 	sigNum,
@@ -2982,29 +2983,6 @@ export class User {
 		return newLeverage;
 	}
 
-	public getUser30dVolume(userStatsAccount: UserStatsAccount, now?: BN) {
-		now = now || new BN(new Date().getTime() / 1000);
-		const sinceLastTaker = BN.max(
-			now.sub(userStatsAccount.lastTakerVolume30DTs),
-			ZERO
-		);
-		const sinceLastMaker = BN.max(
-			now.sub(userStatsAccount.lastMakerVolume30DTs),
-			ZERO
-		);
-		const thirtyDaysInSeconds = new BN(60 * 60 * 24 * 30);
-		const last30dVolume = userStatsAccount.takerVolume30D
-			.mul(BN.max(thirtyDaysInSeconds.sub(sinceLastTaker), ZERO))
-			.div(thirtyDaysInSeconds)
-			.add(
-				userStatsAccount.makerVolume30D
-					.mul((BN.max(thirtyDaysInSeconds.sub(sinceLastMaker), ZERO)))
-					.div(thirtyDaysInSeconds)
-			);
-
-		return last30dVolume;
-	}
-
 	public getUserFeeTier(marketType: MarketType, now?: BN) {
 		const state = this.driftClient.getStateAccount();
 
@@ -3014,7 +2992,10 @@ export class User {
 				.getUserStats()
 				.getAccount();
 
-			const total30dVolume = this.getUser30dVolume(userStatsAccount, now);
+			const total30dVolume = getUser30dRollingVolumeEstimate(
+				userStatsAccount,
+				now
+			);
 
 			const stakedQuoteAssetAmount = userStatsAccount.ifStakedQuoteAssetAmount;
 			const volumeTiers = [
