aeb133593df7d58b39b10ccd5eb246f48d106d2e
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-02-16 08:57:12 -0500

    program: remove-max-market-spread-validation

--- programs/drift/src/validation/perp_market.rs
+++ programs/drift/src/validation/perp_market.rs
@@ -126,29 +126,6 @@ pub fn validate_perp_market(market: &PerpMarket) -> DriftResult {
         market.amm.base_spread
     )?;
 
-    validate!(
-        market
-            .amm
-            .long_spread
-            .safe_add(market.amm.short_spread)?
-            .cast::<u64>()?
-            <= market.amm.max_spread.cast::<u64>()?.max(
-                market
-                    .amm
-                    .last_oracle_reserve_price_spread_pct
-                    .unsigned_abs()
-            ),
-        ErrorCode::InvalidAmmDetected,
-        "long_spread + short_spread > max_spread: {} + {} < {}.max({})",
-        market.amm.long_spread,
-        market.amm.short_spread,
-        market.amm.max_spread,
-        market
-            .amm
-            .last_oracle_reserve_price_spread_pct
-            .unsigned_abs()
-    )?;
-
     if market.amm.base_asset_amount_with_amm > 0 {
         // users are long = removed base and added quote = qar increased
         // bid quote/base < reserve q/b
