ef4f35f13e2b3b468e3d82644125b4534ad125a2
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-03-10 20:10:35 -0400

    add anchor-spl stake

--- programs/drift/src/instructions/validator_stake.rs
+++ programs/drift/src/instructions/validator_stake.rs
@@ -3,15 +3,22 @@ use crate::state::state::State;
 use anchor_lang::prelude::*;
 
 use crate::error::ErrorCode;
-use crate::state::spot_market::SpotMarket;
-use solana_program::msg;
-// use anchor_spl::stake;
 use crate::instructions::constraints::*;
 use crate::load_mut;
+use crate::state::spot_market::SpotMarket;
 use crate::validate;
 use anchor_spl::token::{Token, TokenAccount};
+use solana_program::msg;
 // use solana_program::stake;
 //::instruction::{create_account, delegate_stake, deactivate_stake, withdraw};
+
+use anchor_lang::solana_program::{
+    program::invoke_signed, stake, stake::state::StakeState, system_program,
+};
+use anchor_lang::{prelude::*, solana_program::sysvar::stake_history};
+use anchor_spl::stake::{
+    deactivate_stake, withdraw, DeactivateStake, Stake, StakeAccount, Withdraw,
+};
 
 #[access_control(
     spot_market_valid(&ctx.accounts.spot_market)
@@ -80,6 +87,21 @@ pub fn handle_spot_market_add_to_stake(
     )?;
 
     // todo: add to stake program
+    // invoke(
+    //     &stake::instruction::authorize(
+    //         self.stake_account.to_account_info().key,
+    //         self.stake_authority.key,
+    //         &new_staker,
+    //         StakeAuthorize::Staker,
+    //         None,
+    //     ),
+    //     &[
+    //         self.stake_program.to_account_info(),
+    //         self.stake_account.to_account_info(),
+    //         self.clock.to_account_info(),
+    //         self.stake_authority.to_account_info(),
+    //     ],
+    // )?;
 
     spot_market.staked_token_amount = spot_market.staked_token_amount.safe_add(amount)?;
 
@@ -111,6 +133,19 @@ pub fn handle_spot_market_deactivate_stake(
     )?;
 
     // todo: deactivate amount in stake program
+    // deactivate_stake(CpiContext::new_with_signer(
+    //     ctx.accounts.stake_program.to_account_info(),
+    //     DeactivateStake {
+    //         stake: ctx.accounts.stake_account.to_account_info(),
+    //         staker: ctx.accounts.stake_deposit_authority.to_account_info(),
+    //         clock: ctx.accounts.clock.to_account_info(),
+    //     },
+    //     &[&[
+    //         &self.state.key().to_bytes(),
+    //         StakeSystem::STAKE_DEPOSIT_SEED,
+    //         &[self.state.stake_system.stake_deposit_bump_seed],
+    //     ]],
+    // ))?;
 
     Ok(())
 }
@@ -134,6 +169,15 @@ pub fn handle_spot_market_withdraw_stake(
 
     // saturating since rewards accumulate
     spot_market.staked_token_amount = spot_market.staked_token_amount.saturating_sub(amount);
+
+    // validate invariants
+    ctx.accounts.spot_market_vault.reload()?;
+    ctx.accounts.insurance_fund_vault.reload()?;
+    crate::math::spot_withdraw::validate_spot_market_vault_and_stake_amount(
+        spot_market,
+        ctx.accounts.spot_market_vault.amount,
+        None, // todo: pass amount in stake account
+    )?;
 
     Ok(())
 }
@@ -185,7 +229,12 @@ pub struct AdminUpdateSpotMarketStake<'info> {
         bump,
     )]
     pub insurance_fund_vault: Box<Account<'info, TokenAccount>>,
+    pub system_program: Program<'info, System>,
+    pub stake_program: Program<'info, Stake>,
     pub token_program: Program<'info, Token>,
+    #[account(mut)]
+    pub stake_account: Box<Account<'info, StakeAccount>>,
+    pub stake_authority: Signer<'info>,
 }
 
 #[derive(Accounts)]
@@ -206,5 +255,10 @@ pub struct UpdateSpotMarketStake<'info> {
         bump,
     )]
     pub insurance_fund_vault: Box<Account<'info, TokenAccount>>,
+    pub system_program: Program<'info, System>,
+    pub stake_program: Program<'info, Stake>,
     pub token_program: Program<'info, Token>,
+    #[account(mut)]
+    pub stake_account: Box<Account<'info, StakeAccount>>,
+    pub stake_authority: Signer<'info>,
 }
