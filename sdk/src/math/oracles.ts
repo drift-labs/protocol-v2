17a37ae6d08eb47ced2318a8cebcf97bb5713727
Chris Heaney <chrisheaney30@gmail.com>
2024-03-11 15:30:57 -0400

    add type

--- sdk/src/math/oracles.ts
+++ sdk/src/math/oracles.ts
@@ -27,7 +27,7 @@ export function oraclePriceBands(
 	return [oraclePriceData.price.sub(offset), oraclePriceData.price.add(offset)];
 }
 
-export function getMaxConfidenceIntervalMultiplier(market): BN {
+export function getMaxConfidenceIntervalMultiplier(market: PerpMarketAccount): BN {
 	let maxConfidenceIntervalMultiplier;
 	if (isVariant(market.contractTier, 'a')) {
 		maxConfidenceIntervalMultiplier = new BN(1);
