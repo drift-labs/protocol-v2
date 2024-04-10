11d39ac6a18d0d454cc0524b9dd338df15fb2f96
wphan <william@drift.trade>
2024-04-09 17:18:28 -0700

    better error handling

--- sdk/src/priorityFee/driftPriorityFeeMethod.ts
+++ sdk/src/priorityFee/driftPriorityFeeMethod.ts
@@ -14,9 +14,16 @@ export async function fetchDriftPriorityFee(
 				','
 			)}&marketIndex=${marketIndexs.join(',')}`
 		);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
 		return await response.json();
 	} catch (err) {
-		console.error(err);
+		if (err instanceof Error) {
+			console.error('Error fetching priority fees:', err.message);
+		} else {
+			console.error('Unknown error fetching priority fees:', err);
+		}
 	}
 
 	return [];
