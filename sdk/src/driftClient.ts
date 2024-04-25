d033e5070b396f933618d054295abf2aceb1dd07
lowkeynicc <nick@lunoho.company>
2024-04-25 00:58:21 -0400

    prettify

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -3556,8 +3556,9 @@ export class DriftClient {
 		subAccountId?: number
 	): Promise<TransactionInstruction> {
 		orderParams = getOrderParams(orderParams, { marketType: MarketType.SPOT });
-		const userAccountPublicKey =
-			await this.getUserAccountPublicKey(subAccountId);
+		const userAccountPublicKey = await this.getUserAccountPublicKey(
+			subAccountId
+		);
 
 		const remainingAccounts = this.getRemainingAccounts({
 			userAccounts: [this.getUserAccount(subAccountId)],
@@ -4704,8 +4705,9 @@ export class DriftClient {
 		/* Settle PnL after fill if requested */
 		let settlePnlTx: Transaction;
 		if (settlePnl && isVariant(orderParams.marketType, 'perp')) {
-			const userAccountPublicKey =
-				await this.getUserAccountPublicKey(subAccountId);
+			const userAccountPublicKey = await this.getUserAccountPublicKey(
+				subAccountId
+			);
 
 			const settlePnlIx = await this.settlePNLIx(
 				userAccountPublicKey,
@@ -6276,8 +6278,9 @@ export class DriftClient {
 		}
 
 		if (initializeStakeAccount) {
-			const initializeIx =
-				await this.getInitializeInsuranceFundStakeIx(marketIndex);
+			const initializeIx = await this.getInitializeInsuranceFundStakeIx(
+				marketIndex
+			);
 			addIfStakeIxs.push(initializeIx);
 		}
 
