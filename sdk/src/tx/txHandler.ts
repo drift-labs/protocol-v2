dede753b69dc0471b4a2e58df3f3e7b04097e3bd
Luke Steyn <lukecaan@gmail.com>
2024-06-10 16:08:31 +0800

    Bump

--- sdk/src/tx/txHandler.ts
+++ sdk/src/tx/txHandler.ts
@@ -31,7 +31,7 @@ import { containsComputeUnitIxs } from '../util/computeUnits';
  * When the whileValidTxSender waits for confirmation of a given transaction, it needs the last available blockheight and blockhash used in the signature to do so. For pre-signed transactions, these values aren't attached to the transaction object by default. For a "scrappy" workaround which doesn't break backwards compatibility, the SIGNATURE_BLOCK_AND_EXPIRY property is simply attached to the transaction objects as they are created or signed in this handler despite a mismatch in the typescript types. If the values are attached to the transaction when they reach the whileValidTxSender, it can opt-in to use these values.
  */
 
-const DEV_TRY_FORCE_TX_TIMEOUTS = true;
+const DEV_TRY_FORCE_TX_TIMEOUTS = false;
 
 export const COMPUTE_UNITS_DEFAULT = 200_000;
 
