70e919b537187a3bfd05490268b7378c1d57131a
Norbaeocystin <baranrastislav@gmail.com>
2024-06-12 17:47:42 -0400

    add to ts - getOpenbookV2...

--- sdk/src/addresses/pda.ts
+++ sdk/src/addresses/pda.ts
@@ -236,3 +236,16 @@ export function getPrelaunchOraclePublicKey(
 		programId
 	)[0];
 }
+
+export function getOpenbookV2FulfillmentConfigPublicKey(
+	programId: PublicKey,
+	market: PublicKey
+): PublicKey {
+	return PublicKey.findProgramAddressSync(
+		[
+			Buffer.from(anchor.utils.bytes.utf8.encode('openbook_v2_fulfillment_config')),
+			market.toBuffer(),
+		],
+		programId
+	)[0];
+}
