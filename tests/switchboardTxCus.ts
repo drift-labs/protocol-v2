6b8604fe7a8ae34d19b80a108a511769d374ab77
soundsonacid <soundsonacid@gmail.com>
2024-06-17 11:31:24 -0500

    switchboardTxCus

--- tests/switchboardTxCus.ts
+++ tests/switchboardTxCus.ts
@@ -17,38 +17,31 @@ import { Program } from '@coral-xyz/anchor';
 import { Keypair, PublicKey } from '@solana/web3.js';
 
 import {
-	mockOracle,
+	mockOracleNoProgram,
 	mockUSDCMint,
 	mockUserUSDCAccount,
 	initializeQuoteSpotMarket,
-	printTxLogs,
 } from './testHelpers';
 import {
-	BulkAccountLoader,
-	findComputeUnitConsumption,
 	getOrderParams,
 	MarketType,
 	OrderParams,
 	OrderType,
 	PostOnlyParams,
 } from '../sdk';
+import { startAnchor } from "solana-bankrun";
+import { TestBulkAccountLoader } from '../sdk/src/accounts/testBulkAccountLoader';
+import { BankrunContextWrapper } from '../sdk/src/bankrunConnection';
 
 describe('switchboard place orders cus', () => {
-	const provider = anchor.AnchorProvider.local(undefined, {
-		preflightCommitment: 'confirmed',
-		commitment: 'confirmed',
-	});
-	const connection = provider.connection;
-	anchor.setProvider(provider);
 	const chProgram = anchor.workspace.Drift as Program;
 
 	let driftClient: TestClient;
-	const eventSubscriber = new EventSubscriber(connection, chProgram, {
-		commitment: 'recent',
-	});
-	eventSubscriber.subscribe();
+	let eventSubscriber: EventSubscriber;
 
-	const bulkAccountLoader = new BulkAccountLoader(connection, 'confirmed', 1);
+	let bulkAccountLoader: TestBulkAccountLoader;
+
+	let bankrunContextWrapper: BankrunContextWrapper;
 
 	let usdcMint;
 	let userUSDCAccount;
@@ -73,14 +66,27 @@ describe('switchboard place orders cus', () => {
 	const numMkts = 8;
 
 	before(async () => {
-		usdcMint = await mockUSDCMint(provider);
-		userUSDCAccount = await mockUserUSDCAccount(usdcMint, usdcAmount, provider);
+		const context = await startAnchor("", [], []);
 
-		oracle = await mockOracle(1);
+		bankrunContextWrapper = new BankrunContextWrapper(context);
+
+        bulkAccountLoader = new TestBulkAccountLoader(bankrunContextWrapper.connection, 'processed', 1);
+
+		eventSubscriber = new EventSubscriber(
+			bankrunContextWrapper.connection.toConnection(),
+			chProgram,
+		);
+
+		await eventSubscriber.subscribe();
+
+		usdcMint = await mockUSDCMint(bankrunContextWrapper);
+		userUSDCAccount = await mockUserUSDCAccount(usdcMint, usdcAmount, bankrunContextWrapper);
+
+		oracle = await mockOracleNoProgram(bankrunContextWrapper, 1);
 
 		driftClient = new TestClient({
-			connection,
-			wallet: provider.wallet,
+			connection: bankrunContextWrapper.connection.toConnection(),
+			wallet: bankrunContextWrapper.provider.wallet,
 			programID: chProgram.programId,
 			opts: {
 				commitment: 'confirmed',
@@ -88,6 +94,7 @@ describe('switchboard place orders cus', () => {
 			activeSubAccountId: 0,
 			perpMarketIndexes: [0],
 			spotMarketIndexes: [0],
+			subAccountIds: [],
 			oracleInfos: [
 				{
 					publicKey: oracle,
@@ -141,15 +148,15 @@ describe('switchboard place orders cus', () => {
 			await driftClient.addPerpLpShares(nLpShares.divn(numMkts * 4), i);
 		}
 
-		provider.connection.requestAirdrop(traderKeyPair.publicKey, 10 ** 9);
+		await bankrunContextWrapper.fundKeypair(traderKeyPair, 10 ** 9);
 		traderUSDCAccount = await mockUserUSDCAccount(
 			usdcMint,
 			usdcAmount,
-			provider,
+			bankrunContextWrapper,
 			traderKeyPair.publicKey
 		);
 		traderDriftClient = new TestClient({
-			connection,
+			connection: bankrunContextWrapper.connection.toConnection(),
 			wallet: new Wallet(traderKeyPair),
 			programID: chProgram.programId,
 			opts: {
@@ -158,6 +165,7 @@ describe('switchboard place orders cus', () => {
 			activeSubAccountId: 0,
 			perpMarketIndexes: [0, 1, 2, 3, 4, 5, 6, 7],
 			spotMarketIndexes: [0],
+			subAccountIds: [],
 			oracleInfos: [
 				{
 					publicKey: oracle,
@@ -201,15 +209,9 @@ describe('switchboard place orders cus', () => {
 
 		const txSig = await traderDriftClient.placeOrders(orderParams);
 
-		await printTxLogs(connection, txSig);
+		bankrunContextWrapper.printTxLogs(txSig);
 
-		const cus = (
-			await findComputeUnitConsumption(
-				driftClient.program.programId,
-				driftClient.connection,
-				txSig
-			)
-		)[0];
+		const cus = bankrunContextWrapper.connection.findComputeUnitConsumption(txSig);
 		console.log(cus);
 		assert(cus < 400000);
 	});
