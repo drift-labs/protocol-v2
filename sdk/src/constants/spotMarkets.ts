f15ec5f22e3a7a70dcd76633f2ec48a93c355dc7
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-07-31 11:21:57 -0400

    sdk: spot-market-index-22

--- sdk/src/constants/spotMarkets.ts
+++ sdk/src/constants/spotMarkets.ts
@@ -373,6 +373,17 @@ export const MainnetSpotMarkets: SpotMarketConfig[] = [
 		precisionExp: NINE,
 		launchTs: 1721316817000,
 	},
+	{
+		symbol: 'PYUSD',
+		marketIndex: 22,
+		oracle: new PublicKey('HpMoKp3TCd3QT4MWYUKk2zCBwmhr5Df45fB6wdxYqEeh'),
+		oracleSource: OracleSource.PYTH_PULL,
+		mint: new PublicKey('2apBGMsS6ti9RyF5TwQTDswXBWskiJP2LD4cUEDqYJjk'),
+		precision: new BN(10).pow(SIX),
+		precisionExp: SIX,
+		pythFeedId:
+			'0xc1da1b73d7f01e7ddd54b3766cf7fcd644395ad14f70aa706ec5384c59e76692',
+	},
 ];
 
 export const SpotMarkets: { [key in DriftEnv]: SpotMarketConfig[] } = {
