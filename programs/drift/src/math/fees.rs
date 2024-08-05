1bfabf3ae67d175d4b698ef552a2efd2e95b6167
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-08-05 12:52:24 -0400

    program: update-if-gov-stake-fee-tier

--- programs/drift/src/math/fees.rs
+++ programs/drift/src/math/fees.rs
@@ -414,21 +414,21 @@ fn determine_perp_fee_tier<'a>(
 
     if total_30d_volume >= ONE_HUNDRED_MILLION_QUOTE
         || staked_quote_asset_amount >= TEN_THOUSAND_QUOTE - QUOTE_PRECISION_U64
-        || if_staked_gov_token_amount >= ONE_HUNDRED_THOUSAND_QUOTE + 19_500 * QUOTE_PRECISION_U64
+        || staked_gov_token_amount >= ONE_HUNDRED_THOUSAND_QUOTE + 19_500 * QUOTE_PRECISION_U64
     {
         return Ok(&fee_structure.fee_tiers[5]);
     }
 
     if total_30d_volume >= FIFTY_MILLION_QUOTE
         || staked_quote_asset_amount >= ONE_THOUSAND_QUOTE * 5 - QUOTE_PRECISION_U64
-        || if_staked_gov_token_amount >= ONE_HUNDRED_THOUSAND_QUOTE - QUOTE_PRECISION_U64
+        || staked_gov_token_amount >= ONE_HUNDRED_THOUSAND_QUOTE - QUOTE_PRECISION_U64
     {
         return Ok(&fee_structure.fee_tiers[4]);
     }
 
     if total_30d_volume >= TEN_MILLION_QUOTE
         || staked_quote_asset_amount >= ONE_THOUSAND_QUOTE * 2 - QUOTE_PRECISION_U64
-        || if_staked_gov_token_amount >= TWENTY_FIVE_THOUSAND_QUOTE * 2 - QUOTE_PRECISION_U64
+        || staked_gov_token_amount >= TWENTY_FIVE_THOUSAND_QUOTE * 2 - QUOTE_PRECISION_U64
 
     {
         return Ok(&fee_structure.fee_tiers[3]);
@@ -436,7 +436,7 @@ fn determine_perp_fee_tier<'a>(
 
     if total_30d_volume >= FIVE_MILLION_QUOTE
         || staked_quote_asset_amount >= ONE_THOUSAND_QUOTE - QUOTE_PRECISION_U64
-        || if_staked_gov_token_amount >= TEN_THOUSAND_QUOTE - QUOTE_PRECISION_U64
+        || staked_gov_token_amount >= TEN_THOUSAND_QUOTE - QUOTE_PRECISION_U64
 
     {
         return Ok(&fee_structure.fee_tiers[2]);
@@ -444,7 +444,7 @@ fn determine_perp_fee_tier<'a>(
 
     if total_30d_volume >= ONE_MILLION_QUOTE
         || staked_quote_asset_amount >= ONE_THOUSAND_QUOTE / 2 - QUOTE_PRECISION_U64
-        || if_staked_gov_token_amount >= ONE_THOUSAND_QUOTE - QUOTE_PRECISION_U64
+        || staked_gov_token_amount >= ONE_THOUSAND_QUOTE - QUOTE_PRECISION_U64
 
     {
         return Ok(&fee_structure.fee_tiers[1]);
