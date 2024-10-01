8809674696ef9972b19fdb17b28b741b590d1f64
Luke <lukecaan@gmail.com>
2024-10-01 12:34:31 +0800

    Added new optimised method to await transaction confirmations (#1231)
    * Added new optimised method to await transaction confirmations
    
    * Updated whileValidTxSender to use same tx confirmation logic as other senders
    
    * Minor refactoring and added unit tests for TransactionConfirmationManager
    
    * Prettify fix
    
    * Added some extra safety to transaction confirmation manager

--- sdk/src/tx/whileValidTxSender.ts
+++ sdk/src/tx/whileValidTxSender.ts
@@ -14,8 +14,6 @@ import { DEFAULT_CONFIRMATION_OPTS } from '../config';
 
 const DEFAULT_RETRY = 2000;
 
-const VALID_BLOCK_HEIGHT_OFFSET = -150; // This is a bit of weirdness but the lastValidBlockHeight value returned from connection.getLatestBlockhash is always 300 blocks ahead of the current block, even though the transaction actually expires after 150 blocks. This accounts for that so that we can at least accuractely estimate the transaction expiry.
-
 type ResolveReference = {
 	resolve?: () => void;
 };
@@ -135,7 +133,9 @@ export class WhileValidTxSender extends BaseTxSender {
 		// handle subclass-specific side effects
 		const txSig = bs58.encode(
 			// @ts-expect-error
-			signedTx?.signature || signedTx.signatures[0]?.signature || signedTx.signatures[0]
+			signedTx?.signature ||
+				signedTx.signatures[0]?.signature ||
+				signedTx.signatures[0]
 		);
 		this.untilValid.set(txSig, latestBlockhash);
 
@@ -239,26 +239,9 @@ export class WhileValidTxSender extends BaseTxSender {
 
 		let slot: number;
 		try {
-			const { blockhash, lastValidBlockHeight } = this.untilValid.get(txid);
-
-			const result = await this.connection.confirmTransaction(
-				{
-					signature: txid,
-					blockhash,
-					lastValidBlockHeight: this.useBlockHeightOffset
-						? lastValidBlockHeight + VALID_BLOCK_HEIGHT_OFFSET
-						: lastValidBlockHeight,
-				},
-				opts?.commitment
-			);
-
-			if (!result) {
-				throw new Error(`Couldn't get signature status for txid: ${txid}`);
-			}
+			const result = await this.confirmTransaction(txid, opts.commitment);
 
 			this.txSigCache?.set(txid, true);
-
-			await this.checkConfirmationResultForError(txid, result.value);
 
 			slot = result.context.slot;
 			// eslint-disable-next-line no-useless-catch
