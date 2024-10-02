4b7992549fa1ef068c34761d8bcf2f0df5aff5e7
Nour Alharithi <nour.alharithi@gmail.com>
2024-10-02 12:52:11 -0700

    using anchor test flag

--- programs/drift/src/ids.rs
+++ programs/drift/src/ids.rs
@@ -131,8 +131,7 @@ pub mod admin_hot_wallet {
 
 pub mod swift_server {
     use solana_program::declare_id;
-    #[cfg(feature = "mainnet-beta")]
     declare_id!("SW1fThqrxLzVprnCMpiybiqYQfoNCdduC5uWsSUKChS");
-    #[cfg(not(feature = "mainnet-beta"))]
+    #[cfg(feature = "anchor-test")]
     declare_id!("DpaEdAPW3ZX67fnczT14AoX12Lx9VMkxvtT81nCHy3Nv");
 }
