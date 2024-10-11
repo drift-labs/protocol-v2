bf149c000a5944ac42dc60c97bd7e477546d24f2
DecentralizedDev <181214587+DecentralizedDev@users.noreply.github.com>
2024-10-11 14:09:41 +0400

    Fix issue ordersubscriber unordered updates in select cases

--- sdk/src/orderSubscriber/OrderSubscriber.ts
+++ sdk/src/orderSubscriber/OrderSubscriber.ts
@@ -100,24 +100,14 @@ export class OrderSubscriber {
 
 			const slot: number = rpcResponseAndContext.context.slot;
 
-			const programAccountSet = new Set<string>();
 			for (const programAccount of rpcResponseAndContext.value) {
 				const key = programAccount.pubkey.toString();
-				programAccountSet.add(key);
 				this.tryUpdateUserAccount(
 					key,
 					'raw',
 					programAccount.account.data,
 					slot
 				);
-				// give event loop a chance to breathe
-				await new Promise((resolve) => setTimeout(resolve, 0));
-			}
-
-			for (const key of this.usersAccounts.keys()) {
-				if (!programAccountSet.has(key)) {
-					this.usersAccounts.delete(key);
-				}
 				// give event loop a chance to breathe
 				await new Promise((resolve) => setTimeout(resolve, 0));
 			}
