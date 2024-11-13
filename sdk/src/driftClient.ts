02e7ff289aeea98620f19ac2868579ae94f2ac30
Chris Heaney <chrisheaney30@gmail.com>
2024-11-13 14:44:23 -0500

    change sdk default

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -5565,7 +5565,7 @@ export class DriftClient {
 
 		let optionalParams = null;
 		if (auctionDurationPercentage || successCondition) {
-			optionalParams = (successCondition ?? 0) << 8 | (auctionDurationPercentage ?? 0);
+			optionalParams = (successCondition ?? 0) << 8 | (auctionDurationPercentage ?? 100);
 		}
 
 		return await this.program.instruction.placeAndTakePerpOrder(
