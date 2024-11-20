4cb1812f9b479a798b08f998bebdb38f68ceb3e7
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-11-20 14:47:59 -0500

    program: can-lower-k-improvement

--- programs/drift/src/state/perp_market.rs
+++ programs/drift/src/state/perp_market.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 
 use crate::state::state::State;
+use crate::BASE_PRECISION;
 use std::cmp::max;
 
 use crate::controller::position::{PositionDelta, PositionDirection};
@@ -1398,10 +1399,12 @@ impl AMM {
 
     pub fn can_lower_k(&self) -> DriftResult<bool> {
         let (max_bids, max_asks) = amm::calculate_market_open_bids_asks(self)?;
-        let can_lower = self.base_asset_amount_with_amm.unsigned_abs()
-            < max_bids.unsigned_abs().min(max_asks.unsigned_abs())
-            && self.base_asset_amount_with_amm.unsigned_abs()
-                < self.sqrt_k.safe_sub(self.user_lp_shares)?;
+        let can_lower = (self.base_asset_amount_with_amm.unsigned_abs()
+            < max_bids.unsigned_abs().min(max_asks.unsigned_abs()))
+            && (self.base_asset_amount_with_amm.unsigned_abs()
+                < self.sqrt_k.safe_sub(self.user_lp_shares)?)
+            && (self.min_order_size.cast::<u128>()? < max_bids.unsigned_abs().max(max_asks.unsigned_abs()));
+
         Ok(can_lower)
     }
 
