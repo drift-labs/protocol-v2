1026696cd9bc64087cec6a2b85482e6e5cc58012
Chris Heaney <chrisheaney30@gmail.com>
2025-01-02 12:06:22 -0500

    add calculateMaxPctToLiquidate

--- sdk/src/math/liquidation.ts
+++ sdk/src/math/liquidation.ts
@@ -4,6 +4,8 @@ import {
 	LIQUIDATION_FEE_PRECISION,
 	MARGIN_PRECISION,
 	PRICE_TIMES_AMM_TO_QUOTE_PRECISION_RATIO,
+	QUOTE_PRECISION,
+	LIQUIDATION_PCT_PRECISION,
 } from '../constants/numericConstants';
 
 export function calculateBaseAssetAmountToCoverMarginShortage(
@@ -34,4 +36,46 @@ export function calculateBaseAssetAmountToCoverMarginShortage(
 				oraclePrice.mul(new BN(ifLiquidationFee)).div(LIQUIDATION_FEE_PRECISION)
 			)
 	);
+}
+
+export function calculateMaxPctToLiquidate(
+	userLastActiveSlot: BN,
+	userLiquidationMarginFreed: BN,
+	marginShortage: BN,
+	slot: BN,
+	initialPctToLiquidate: BN,
+	liquidationDuration: BN
+): BN {
+	// if margin shortage is tiny, accelerate liquidation
+	if (marginShortage.lt(new BN(50).mul(QUOTE_PRECISION))) {
+		return LIQUIDATION_PCT_PRECISION;
+	}
+
+	let slotsElapsed;
+	if (userLiquidationMarginFreed.gt(new BN(0))) {
+		slotsElapsed = BN.max(slot.sub(userLastActiveSlot), new BN(0));
+	} else {
+		slotsElapsed = new BN(0);
+	}
+
+	const pctFreeable = BN.min(
+		slotsElapsed
+			.mul(LIQUIDATION_PCT_PRECISION)
+			.div(liquidationDuration) // ~ 1 minute if per slot is 400ms
+			.add(initialPctToLiquidate),
+		LIQUIDATION_PCT_PRECISION
+	);
+
+	const totalMarginShortage = marginShortage.add(userLiquidationMarginFreed);
+	const maxMarginFreed = totalMarginShortage
+		.mul(pctFreeable)
+		.div(LIQUIDATION_PCT_PRECISION);
+	const marginFreeable = BN.max(
+		maxMarginFreed.sub(userLiquidationMarginFreed),
+		new BN(0)
+	);
+
+	return marginFreeable
+		.mul(LIQUIDATION_PCT_PRECISION)
+		.div(marginShortage);
 }
