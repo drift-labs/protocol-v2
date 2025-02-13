c4885c96360e98c226c9745bba0c1de80b8d7b7e
DecentralizedDev <181214587+DecentralizedDev@users.noreply.github.com>
2025-02-13 11:06:34 +0100

    Also add check in sort value

--- sdk/src/dlob/DLOBNode.ts
+++ sdk/src/dlob/DLOBNode.ts
@@ -113,7 +113,7 @@ export class RestingLimitOrderNode extends OrderNode {
 
 	getSortValue(order: Order): BN {
 		let sortValue = order.price;
-		if (this.applyProtectedMakerOffset) {
+		if (this.applyProtectedMakerOffset && this.isProtectedMaker) {
 			const offset = sortValue.divn(1000);
 
 			if (isVariant(order.direction, 'long')) {
