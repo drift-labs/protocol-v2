4ebc3afb894e7e009dffe4586d782824016d4455
Nour Alharithi <nour.alharithi@gmail.com>
2025-03-20 16:28:11 -0700

    remove unnecessary undefined check

--- sdk/src/swift/swiftOrderSubscriber.ts
+++ sdk/src/swift/swiftOrderSubscriber.ts
@@ -141,8 +141,7 @@ export class SwiftOrderSubscriber {
 						| SignedMsgOrderParamsDelegateMessage =
 						this.driftClient.decodeSignedMsgOrderParamsMessage(
 							signedMsgOrderParamsBuf,
-							order['signing_authority'] &&
-								order['signing_authority'] != order['taker_authority']
+							order['signing_authority'] != order['taker_authority']
 						);
 
 					if (!signedMsgOrderParamsMessage.signedMsgOrderParams.price) {
