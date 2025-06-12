9f838a23500f9f2282ec1809522de60de40e3a1b
Chris Heaney <chrisheaney30@gmail.com>
2025-06-12 18:55:34 -0400

    fix cargo test

--- programs/drift/src/controller/orders/tests.rs
+++ programs/drift/src/controller/orders/tests.rs
@@ -268,7 +268,7 @@ pub mod fill_order_protected_maker {
         .unwrap();
 
         assert_eq!(base_asset_amount, 1000000000);
-        assert_eq!(quote_asset_amount, 100_000_000); // $100 + 10 bps
+        assert_eq!(quote_asset_amount, 100100000); // $100 + 10 bps
 
         // user exempt, no 10 bps applied for pmm
         let mut taker_stats = UserStats {
