870075a9150fdb862140123ecc0ebe894b2876d8
Lukas deConantsesznak <lukas@lunoho.company>
2025-06-13 13:36:48 -0600

    feat: PR feedback on customized loader cleaup code and better naming

--- sdk/src/accounts/customizedCadenceBulkAccountLoader.ts
+++ sdk/src/accounts/customizedCadenceBulkAccountLoader.ts
@@ -1,5 +1,7 @@
+import { GET_MULTIPLE_ACCOUNTS_CHUNK_SIZE } from '../constants/numericConstants';
 import { BulkAccountLoader } from './bulkAccountLoader';
 import { Commitment, Connection, PublicKey } from '@solana/web3.js';
+import { v4 as uuidv4 } from 'uuid';
 
 export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 	private customIntervalIds: Map<string, NodeJS.Timeout>;
@@ -17,7 +19,7 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 		this.defaultPollingFrequency = defaultPollingFrequency;
 	}
 
-	private updateCustomPolling(frequency: number): void {
+	private reloadFrequencyGroup(frequency: number): void {
 		const frequencyStr = frequency.toString();
 		const existingInterval = this.customIntervalIds.get(frequencyStr);
 		if (existingInterval) {
@@ -27,15 +29,29 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 
 		const group = this.customPollingGroups.get(frequency);
 		if (group && group.size > 0) {
-			const intervalId = setInterval(async () => {
+
+			const handleAccountLoading = async () => {
 				const accounts = Array.from(group)
 					.map((key) => this.accountsToLoad.get(key))
 					.filter((account) => account !== undefined);
 
 				if (accounts.length > 0) {
-					await this.loadChunk([accounts]);
+					const chunks = this.chunks(
+						this.chunks(
+							Array.from(accounts),
+							GET_MULTIPLE_ACCOUNTS_CHUNK_SIZE
+						),
+						10
+					);
+		
+					await Promise.all(
+						chunks.map((chunk) => {
+							return this.loadChunk(chunk);
+						})
+					);
 				}
-			}, frequency);
+			}
+			const intervalId = setInterval(handleAccountLoading, frequency);
 			this.customIntervalIds.set(frequencyStr, intervalId);
 		}
 	}
@@ -46,9 +62,14 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 	): void {
 		const key = publicKey.toBase58();
 
+		let removedFromOldGroup = false;
 		// Remove from old frequency group
 		for (const [frequency, group] of this.customPollingGroups.entries()) {
 			if (group.has(key)) {
+				if(newFrequency === frequency) {
+					// if frequency is the same, we do nothing
+					break;
+				}
 				group.delete(key);
 				if (group.size === 0) {
 					const intervalId = this.customIntervalIds.get(frequency.toString());
@@ -58,20 +79,23 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 					}
 					this.customPollingGroups.delete(frequency);
 				}
-				this.updateCustomPolling(frequency);
+				removedFromOldGroup = true;
 				break;
 			}
 		}
 
 		// Add to new frequency group
-		let group = this.customPollingGroups.get(newFrequency);
-		if (!group) {
-			group = new Set();
-			this.customPollingGroups.set(newFrequency, group);
-		}
-		group.add(key);
+		if(removedFromOldGroup) {
 
-		this.updateCustomPolling(newFrequency);
+			let group = this.customPollingGroups.get(newFrequency);
+			if (!group) {
+				group = new Set();
+				this.customPollingGroups.set(newFrequency, group);
+			}
+			group.add(key);
+			
+			this.reloadFrequencyGroup(newFrequency);
+		}
 	}
 
 	public async addAccount(
@@ -79,7 +103,17 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 		callback: (buffer: Buffer, slot: number) => void,
 		customPollingFrequency?: number
 	): Promise<string> {
-		const id = await super.addAccount(publicKey, callback);
+		const callbackId = uuidv4();
+		const callbacks = new Map<
+				string,
+				(buffer: Buffer, slot: number) => void
+			>();
+			callbacks.set(callbackId, callback);
+		const newAccountToLoad = {
+			publicKey,
+			callbacks,
+		};
+		this.accountsToLoad.set(publicKey.toString(), newAccountToLoad);
 
 		const key = publicKey.toBase58();
 		const frequency = customPollingFrequency || this.defaultPollingFrequency;
@@ -92,9 +126,9 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 		}
 		group.add(key);
 
-		this.updateCustomPolling(frequency);
+		this.reloadFrequencyGroup(frequency);
 
-		return id;
+		return callbackId;
 	}
 
 	public removeAccount(publicKey: PublicKey, id?: string): void {
@@ -114,7 +148,7 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 					}
 					this.customPollingGroups.delete(frequency);
 				}
-				this.updateCustomPolling(frequency);
+				this.reloadFrequencyGroup(frequency);
 				break;
 			}
 		}
@@ -131,10 +165,10 @@ export class CustomizedCadenceBulkAccountLoader extends BulkAccountLoader {
 	}
 
 	public startPolling(): void {
-		// Don't start the default polling interval
-		// Only start custom polling for accounts that have custom frequencies
+		// Don't start the polling in the base class
+		// Only start polling in these custom frequencies
 		for (const frequency of this.customPollingGroups.keys()) {
-			this.updateCustomPolling(frequency);
+			this.reloadFrequencyGroup(frequency);
 		}
 	}
 
--- sdk/src/constants/numericConstants.ts
+++ sdk/src/constants/numericConstants.ts
@@ -114,3 +114,5 @@ export const FUEL_WINDOW = new BN(60 * 60 * 24 * 28); // 28 days
 export const FUEL_START_TS = new BN(1723147200); // unix timestamp
 
 export const MAX_PREDICTION_PRICE = PRICE_PRECISION;
+
+export const GET_MULTIPLE_ACCOUNTS_CHUNK_SIZE = 99;
