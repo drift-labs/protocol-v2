b180bcfbb3ce9b2964fb34726f3421fff9ce778a
wphan <william@drift.trade>
2025-06-17 08:58:44 -0700

    sdk: allow custom coder

--- sdk/src/testClient.ts
+++ sdk/src/testClient.ts
@@ -3,14 +3,19 @@ import { ConfirmOptions, Signer, Transaction } from '@solana/web3.js';
 import { TxSigAndSlot } from './tx/types';
 import { PollingDriftClientAccountSubscriber } from './accounts/pollingDriftClientAccountSubscriber';
 import { DriftClientConfig } from './driftClientConfig';
+import { AccountsCoder } from '@coral-xyz/anchor/dist/cjs/coder';
 
 export class TestClient extends AdminClient {
-	public constructor(config: DriftClientConfig) {
+	public constructor(config: DriftClientConfig, accountsCoder?: AccountsCoder) {
 		config.txVersion = 'legacy';
 		if (config.accountSubscription.type !== 'polling') {
 			throw new Error('Test client must be polling');
 		}
 		super(config);
+		if (accountsCoder) {
+			// @ts-ignore
+			this.program.coder.accounts = accountsCoder;
+		}
 	}
 
 	async sendTransaction(
