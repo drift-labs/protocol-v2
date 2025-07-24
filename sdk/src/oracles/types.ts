d812fd21fe08228ad42affd17a2c61c85bf21935
Nour Alharithi <nour.alharithi@gmail.com>
2025-07-24 12:26:11 -0700

    updated type

--- sdk/src/oracles/types.ts
+++ sdk/src/oracles/types.ts
@@ -16,7 +16,7 @@ export type OraclePriceData = {
 	twap?: BN;
 	twapConfidence?: BN;
 	maxPrice?: BN; // pre-launch markets only
-	fetchedWithMMOacle?: boolean;
+	fetchedWithMMOracle?: boolean;
 };
 
 export type OracleInfo = {
