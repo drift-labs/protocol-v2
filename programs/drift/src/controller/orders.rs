04da993d0ef2c9f322b1abab8b04bcfe6fa5f551
Chris Heaney <chrisheaney30@gmail.com>
2025-07-28 20:00:06 -0400

    tweak

--- programs/drift/src/controller/orders.rs
+++ programs/drift/src/controller/orders.rs
@@ -210,7 +210,9 @@ pub fn place_perp_order(
             market.amm.order_step_size
         )?;
 
-        let base_asset_amount = if params.base_asset_amount == u64::MAX && !params.reduce_only {
+        let base_asset_amount = if params.base_asset_amount == u64::MAX
+            && !(params.is_trigger_order() && params.reduce_only)
+        {
             calculate_max_perp_order_size(
                 user,
                 position_index,
@@ -3623,7 +3625,9 @@ pub fn place_spot_order(
             step_size
         )?;
 
-        let base_asset_amount = if params.base_asset_amount == u64::MAX && !params.reduce_only {
+        let base_asset_amount = if params.base_asset_amount == u64::MAX
+            && !(params.is_trigger_order() && params.reduce_only)
+        {
             calculate_max_spot_order_size(
                 user,
                 params.market_index,
