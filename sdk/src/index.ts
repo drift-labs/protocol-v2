62712bb85d6435b48442a44e2e6bddb732758788
Lukas deConantsesznak <lukas@lunoho.company>
2025-07-30 12:13:56 -0600

    fix: prettier fixed

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -12,7 +12,7 @@ export * from './accounts/fetch';
 export * from './accounts/webSocketDriftClientAccountSubscriber';
 export * from './accounts/webSocketInsuranceFundStakeAccountSubscriber';
 export * from './accounts/webSocketHighLeverageModeConfigAccountSubscriber';
-export { WebSocketAccountSubscriberV2 } from './accounts/webSocketAccountSubscriberV2'; 
+export { WebSocketAccountSubscriberV2 } from './accounts/webSocketAccountSubscriberV2';
 export * from './accounts/bulkAccountLoader';
 export * from './accounts/bulkUserSubscription';
 export * from './accounts/bulkUserStatsSubscription';
