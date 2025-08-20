6523af06380f1d2d90afb9b4759bf81df706d52d
Lukas deConantsesznak <lukas@lunoho.company>
2025-08-20 13:32:30 -0600

    feat: optional swift sign message fn

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -6382,7 +6382,8 @@ export class DriftClient {
 		orderParamsMessage:
 			| SignedMsgOrderParamsMessage
 			| SignedMsgOrderParamsDelegateMessage,
-		delegateSigner?: boolean
+		delegateSigner?: boolean,
+		signMessageFunction?: (message: Uint8Array) => Buffer
 	): SignedMsgOrderParams {
 		const borshBuf = this.encodeSignedMsgOrderParamsMessage(
 			orderParamsMessage,
@@ -6391,7 +6392,9 @@ export class DriftClient {
 		const orderParams = Buffer.from(borshBuf.toString('hex'));
 		return {
 			orderParams,
-			signature: this.signMessage(Buffer.from(borshBuf.toString('hex'))),
+			signature: signMessageFunction
+				? signMessageFunction(Buffer.from(borshBuf.toString('hex')))
+				: this.signMessage(Buffer.from(borshBuf.toString('hex'))),
 		};
 	}
 
