4973dfb99290841377c0b45832780bfbdb42d77e
0xbigz <83473873+0xbigz@users.noreply.github.com>
2025-08-28 10:59:57 -0400

    revert .sh

--- test-scripts/single-anchor-test.sh
+++ test-scripts/single-anchor-test.sh
@@ -6,10 +6,7 @@ fi
 
 export ANCHOR_WALLET=~/.config/solana/id.json
 
-test_files=(
-	lpPool.ts
-	# lpPoolSwap.ts
-)
+test_files=(admin.ts)
 
 for test_file in ${test_files[@]}; do
   ts-mocha -t 300000 ./tests/${test_file}
