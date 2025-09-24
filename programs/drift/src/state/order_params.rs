e592e676089613f1290560adcecb65b05a66cf62
wphan <william@drift.trade>
2025-09-23 22:37:41 -0700

    Revert "Revert "Crispeaney/revert swift max margin ratio" (#1877)" (#1907)
    This reverts commit 0a8e15349f45e135df3eb2341f163d70ef09fe64.

--- programs/drift/src/state/order_params.rs
+++ programs/drift/src/state/order_params.rs
@@ -872,7 +872,6 @@ pub struct SignedMsgOrderParamsMessage {
     pub uuid: [u8; 8],
     pub take_profit_order_params: Option<SignedMsgTriggerOrderParams>,
     pub stop_loss_order_params: Option<SignedMsgTriggerOrderParams>,
-    pub max_margin_ratio: Option<u16>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Default, Eq, PartialEq, Debug)]
@@ -883,7 +882,6 @@ pub struct SignedMsgOrderParamsDelegateMessage {
     pub uuid: [u8; 8],
     pub take_profit_order_params: Option<SignedMsgTriggerOrderParams>,
     pub stop_loss_order_params: Option<SignedMsgTriggerOrderParams>,
-    pub max_margin_ratio: Option<u16>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Default, Eq, PartialEq, Debug)]
