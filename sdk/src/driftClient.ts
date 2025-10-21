4c85950683afcc94fd5cb569439197fa5a2962db
Nour Alharithi <14929853+moosecat2@users.noreply.github.com>
2025-10-21 10:28:35 -0700

    prettify

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -353,8 +353,8 @@ export class DriftClient {
 		this.authoritySubAccountMap = config.authoritySubAccountMap
 			? config.authoritySubAccountMap
 			: config.subAccountIds
-				? new Map([[this.authority.toString(), config.subAccountIds]])
-				: new Map<string, number[]>();
+			? new Map([[this.authority.toString(), config.subAccountIds]])
+			: new Map<string, number[]>();
 
 		this.includeDelegates = config.includeDelegates ?? false;
 		if (config.accountSubscription?.type === 'polling') {
@@ -844,8 +844,8 @@ export class DriftClient {
 		this.authoritySubAccountMap = authoritySubaccountMap
 			? authoritySubaccountMap
 			: subAccountIds
-				? new Map([[this.authority.toString(), subAccountIds]])
-				: new Map<string, number[]>();
+			? new Map([[this.authority.toString(), subAccountIds]])
+			: new Map<string, number[]>();
 
 		/* Reset user stats account */
 		if (this.userStats?.isSubscribed) {
@@ -997,7 +997,7 @@ export class DriftClient {
 					[...this.authoritySubAccountMap.values()][0][0] ?? 0,
 					new PublicKey(
 						[...this.authoritySubAccountMap.keys()][0] ??
-						this.authority.toString()
+							this.authority.toString()
 					)
 				);
 			}
@@ -3316,19 +3316,19 @@ export class DriftClient {
 
 		const depositCollateralIx = isFromSubaccount
 			? await this.getTransferDepositIx(
-				amount,
-				marketIndex,
-				fromSubAccountId,
-				subAccountId
-			)
+					amount,
+					marketIndex,
+					fromSubAccountId,
+					subAccountId
+			  )
 			: await this.getDepositInstruction(
-				amount,
-				marketIndex,
-				userTokenAccount,
-				subAccountId,
-				false,
-				false
-			);
+					amount,
+					marketIndex,
+					userTokenAccount,
+					subAccountId,
+					false,
+					false
+			  );
 
 		if (subAccountId === 0) {
 			if (
@@ -4368,14 +4368,14 @@ export class DriftClient {
 
 		const marketOrderTxIxs = positionMaxLev
 			? this.getPlaceOrdersAndSetPositionMaxLevIx(
-				[orderParams, ...bracketOrdersParams],
-				positionMaxLev,
-				userAccount.subAccountId
-			)
+					[orderParams, ...bracketOrdersParams],
+					positionMaxLev,
+					userAccount.subAccountId
+			  )
 			: this.getPlaceOrdersIx(
-				[orderParams, ...bracketOrdersParams],
-				userAccount.subAccountId
-			);
+					[orderParams, ...bracketOrdersParams],
+					userAccount.subAccountId
+			  );
 
 		ixPromisesForTxs.marketOrderTx = marketOrderTxIxs;
 
@@ -4523,10 +4523,10 @@ export class DriftClient {
 
 		const user = isDepositToTradeTx
 			? getUserAccountPublicKeySync(
-				this.program.programId,
-				this.authority,
-				subAccountId
-			)
+					this.program.programId,
+					this.authority,
+					subAccountId
+			  )
 			: await this.getUserAccountPublicKey(subAccountId);
 
 		const remainingAccounts = this.getRemainingAccounts({
@@ -5163,14 +5163,14 @@ export class DriftClient {
 		const marketIndex = order
 			? order.marketIndex
 			: userAccount.orders.find(
-				(order) => order.orderId === userAccount.nextOrderId - 1
-			).marketIndex;
+					(order) => order.orderId === userAccount.nextOrderId - 1
+			  ).marketIndex;
 
 		makerInfo = Array.isArray(makerInfo)
 			? makerInfo
 			: makerInfo
-				? [makerInfo]
-				: [];
+			? [makerInfo]
+			: [];
 
 		const userAccounts = [userAccount];
 		for (const maker of makerInfo) {
@@ -5386,14 +5386,14 @@ export class DriftClient {
 		const marketIndex = order
 			? order.marketIndex
 			: userAccount.orders.find(
-				(order) => order.orderId === userAccount.nextOrderId - 1
-			).marketIndex;
+					(order) => order.orderId === userAccount.nextOrderId - 1
+			  ).marketIndex;
 
 		makerInfo = Array.isArray(makerInfo)
 			? makerInfo
 			: makerInfo
-				? [makerInfo]
-				: [];
+			? [makerInfo]
+			: [];
 
 		const userAccounts = [userAccount];
 		for (const maker of makerInfo) {
@@ -6732,8 +6732,8 @@ export class DriftClient {
 		makerInfo = Array.isArray(makerInfo)
 			? makerInfo
 			: makerInfo
-				? [makerInfo]
-				: [];
+			? [makerInfo]
+			: [];
 
 		const userAccounts = [this.getUserAccount(subAccountId)];
 		for (const maker of makerInfo) {
@@ -6979,13 +6979,13 @@ export class DriftClient {
 			prefix,
 			delegateSigner
 				? this.program.coder.types.encode(
-					'SignedMsgOrderParamsDelegateMessage',
-					withBuilderDefaults as SignedMsgOrderParamsDelegateMessage
-				)
+						'SignedMsgOrderParamsDelegateMessage',
+						withBuilderDefaults as SignedMsgOrderParamsDelegateMessage
+				  )
 				: this.program.coder.types.encode(
-					'SignedMsgOrderParamsMessage',
-					withBuilderDefaults as SignedMsgOrderParamsMessage
-				),
+						'SignedMsgOrderParamsMessage',
+						withBuilderDefaults as SignedMsgOrderParamsMessage
+				  ),
 		]);
 		return buf;
 	}
@@ -10710,8 +10710,8 @@ export class DriftClient {
 	): Promise<TransactionInstruction> {
 		const remainingAccounts = userAccount
 			? this.getRemainingAccounts({
-				userAccounts: [userAccount],
-			})
+					userAccounts: [userAccount],
+			  })
 			: undefined;
 
 		const ix = await this.program.instruction.disableUserHighLeverageMode(
