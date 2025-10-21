f8c6e87535309b2736569c6d4319fbc609583049
Lukas deConantsesznak <lukas@lunoho.company>
2025-10-21 10:48:08 -0600

    fix: prettier

--- sdk/src/driftClient.ts
+++ sdk/src/driftClient.ts
@@ -7358,12 +7358,12 @@ export class DriftClient {
 		const isDelegateSigner = takerInfo.signingAuthority.equals(
 			takerInfo.takerUserAccount.delegate
 		);
-		
+
 		const borshBuf = Buffer.from(
 			signedSignedMsgOrderParams.orderParams.toString(),
 			'hex'
 		);
-		
+
 		const signedMessage = this.decodeSignedMsgOrderParamsMessage(
 			borshBuf,
 			isDelegateSigner
@@ -7373,7 +7373,11 @@ export class DriftClient {
 			userAccounts: [takerInfo.takerUserAccount],
 			useMarketLastSlotCache: false,
 			readablePerpMarketIndex: marketIndex,
-			writableSpotMarketIndexes: signedMessage.isolatedPositionDeposit?.gt(new BN(0)) ? [QUOTE_SPOT_MARKET_INDEX] : undefined,
+			writableSpotMarketIndexes: signedMessage.isolatedPositionDeposit?.gt(
+				new BN(0)
+			)
+				? [QUOTE_SPOT_MARKET_INDEX]
+				: undefined,
 		});
 
 		if (isUpdateHighLeverageMode(signedMessage.signedMsgOrderParams.bitFlags)) {
