a53ee3d1370d696a7b9e23e517ba9a3f35646234
Chris Heaney <chrisheaney30@gmail.com>
2025-12-01 16:17:48 -0500

    cargo fmt --

--- programs/drift/src/instructions/keeper.rs
+++ programs/drift/src/instructions/keeper.rs
@@ -475,7 +475,11 @@ pub fn handle_update_user_idle<'c: 'info, 'info>(
 
     let mut updated_lp_fields = false;
     for perp_position in user.perp_positions.iter_mut() {
-        if perp_position.lp_shares != 0 || perp_position.last_base_asset_amount_per_lp != 0 || perp_position.last_quote_asset_amount_per_lp != 0 || perp_position.per_lp_base != 0 {
+        if perp_position.lp_shares != 0
+            || perp_position.last_base_asset_amount_per_lp != 0
+            || perp_position.last_quote_asset_amount_per_lp != 0
+            || perp_position.per_lp_base != 0
+        {
             perp_position.lp_shares = 0;
             perp_position.last_base_asset_amount_per_lp = 0;
             perp_position.last_quote_asset_amount_per_lp = 0;
