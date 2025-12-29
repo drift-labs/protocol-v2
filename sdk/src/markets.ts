aa5dafda3131f591ecd5b86605d3b49c434d3187
LukasDeco <ldeconantsesznak@gmail.com>
2025-12-29 16:17:31 -0700

    fix: prettier

--- sdk/src/markets.ts
+++ sdk/src/markets.ts
@@ -3,12 +3,22 @@ import { SpotMarketConfig, SpotMarkets } from './constants';
 import { PerpMarketConfig, PerpMarkets } from './constants/perpMarkets';
 import { isOneOfVariant } from './types';
 
-export const getActivePerpMarkets = (driftEnv: DriftEnv): PerpMarketConfig[] => {
-	return PerpMarkets[driftEnv ?? 'mainnet-beta'].filter((market) => (!market.marketStatus ||
-        !isOneOfVariant(market.marketStatus, ['delisted', 'settlement'])));
+export const getActivePerpMarkets = (
+	driftEnv: DriftEnv
+): PerpMarketConfig[] => {
+	return PerpMarkets[driftEnv ?? 'mainnet-beta'].filter(
+		(market) =>
+			!market.marketStatus ||
+			!isOneOfVariant(market.marketStatus, ['delisted', 'settlement'])
+	);
 };
 
-export const getActiveSpotMarkets = (driftEnv: DriftEnv): SpotMarketConfig[] => {
-	return SpotMarkets[driftEnv ?? 'mainnet-beta'].filter((market) => (!market.marketStatus ||
-        !isOneOfVariant(market.marketStatus, ['delisted', 'settlement'])));
+export const getActiveSpotMarkets = (
+	driftEnv: DriftEnv
+): SpotMarketConfig[] => {
+	return SpotMarkets[driftEnv ?? 'mainnet-beta'].filter(
+		(market) =>
+			!market.marketStatus ||
+			!isOneOfVariant(market.marketStatus, ['delisted', 'settlement'])
+	);
 };
