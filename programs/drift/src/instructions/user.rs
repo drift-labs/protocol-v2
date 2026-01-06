4abeef67983e44e75fbb60f240571ade5f40c557
Chris Heaney <chrisheaney30@gmail.com>
2026-01-06 13:40:27 -0500

    program: relax whitelist check for init user

--- programs/drift/src/instructions/user.rs
+++ programs/drift/src/instructions/user.rs
@@ -239,8 +239,9 @@ pub fn handle_initialize_user<'c: 'info, 'info>(
         )?;
     }
 
+    let authority_is_signer = ctx.accounts.authority.is_signer;
     #[cfg(feature = "mainnet-beta")]
-    if ctx.accounts.authority.key() != ctx.accounts.payer.key() {
+    if !authority_is_signer && ctx.accounts.authority.key() != ctx.accounts.payer.key() {
         validate!(
             WHITELISTED_EXTERNAL_DEPOSITORS.contains(&ctx.accounts.payer.key()),
             ErrorCode::DefaultError,
@@ -283,8 +284,9 @@ pub fn handle_initialize_user_stats<'c: 'info, 'info>(
         ErrorCode::MaxNumberOfUsers
     )?;
 
+    let authority_is_signer = ctx.accounts.authority.is_signer;
     #[cfg(feature = "mainnet-beta")]
-    if ctx.accounts.authority.key() != ctx.accounts.payer.key() {
+    if !authority_is_signer && ctx.accounts.authority.key() != ctx.accounts.payer.key() {
         validate!(
             WHITELISTED_EXTERNAL_DEPOSITORS.contains(&ctx.accounts.payer.key()),
             ErrorCode::DefaultError,
