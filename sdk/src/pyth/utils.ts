be1f275e06af49b91bc4070c96f138e577c0b92e
LukasDeco <ldeconantsesznak@gmail.com>
2026-01-12 10:23:51 -0700

    fix: prettier

--- sdk/src/pyth/utils.ts
+++ sdk/src/pyth/utils.ts
@@ -1,13 +1,13 @@
 import { PublicKey } from '@solana/web3.js';
 
 export const getGuardianSetPda = (
-    guardianSetIndex: number,
-    wormholeProgramId: PublicKey,
-  ) => {
-    const guardianSetIndexBuf = Buffer.alloc(4);
-    guardianSetIndexBuf.writeUInt32BE(guardianSetIndex, 0);
-    return PublicKey.findProgramAddressSync(
-      [Buffer.from("GuardianSet"), guardianSetIndexBuf],
-      wormholeProgramId,
-    )[0];
-  };
+	guardianSetIndex: number,
+	wormholeProgramId: PublicKey
+) => {
+	const guardianSetIndexBuf = Buffer.alloc(4);
+	guardianSetIndexBuf.writeUInt32BE(guardianSetIndex, 0);
+	return PublicKey.findProgramAddressSync(
+		[Buffer.from('GuardianSet'), guardianSetIndexBuf],
+		wormholeProgramId
+	)[0];
+};
